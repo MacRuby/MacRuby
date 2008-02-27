95838ddd132fba4c3d4358a7937d5d1cd183a47d
Laurent Sansonetti <lsansonetti@apple.com>
2008-02-27 06:32:34 +0000

    fixed xcode template, now building
    git-svn-id: http://svn.macosforge.org/repository/ruby/MacRuby/trunk@17 23306eb0-4c56-4727-a40e-e92c0eb68959

--- misc/xcode-templates/Project Templates/Application/MacRuby Application/rb_main.rb
+++ misc/xcode-templates/Project Templates/Application/MacRuby Application/rb_main.rb
@@ -1,9 +1,9 @@
 #
 # rb_main.rb
-# Â«PROJECTNAMEÂ»
+# ÇPROJECTNAMEÈ
 #
-# Created by Â«FULLUSERNAMEÂ» on Â«DATEÂ».
-# Copyright Â«ORGANIZATIONNAMEÂ» Â«YEARÂ». All rights reserved.
+# Created by ÇFULLUSERNAMEÈ on ÇDATEÈ.
+# Copyright ÇORGANIZATIONNAMEÈ ÇYEARÈ. All rights reserved.
 #
 
 # Loading the Cocoa framework. If you need to load more frameworks, you can
@@ -13,7 +13,7 @@ framework 'cocoa'
 # Loading all the Ruby project files.
 dir_path = NSBundle.mainBundle.resourcePath.fileSystemRepresentation
 Dir.entries(dir_path).each do |path|
-  if path != File.basename(__FILE__) and path [-3..-1] == '.rb'
+  if path != File.basename(__FILE__) and path[-3..-1] == '.rb'
     require(path)
   end
 end
