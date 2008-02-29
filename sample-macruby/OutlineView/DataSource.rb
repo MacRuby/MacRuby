6646f88390e93a06995c8cb43261979bfaa4c01b
Laurent Sansonetti <lsansonetti@apple.com>
2008-02-29 23:08:17 +0000

    added OutlineView sample
    git-svn-id: http://svn.macosforge.org/repository/ruby/MacRuby/trunk@33 23306eb0-4c56-4727-a40e-e92c0eb68959

---
+++ sample-macruby/OutlineView/DataSource.rb
@@ -0,0 +1,23 @@
+class DataSource
+
+  def outlineView outlineView, numberOfChildrenOfItem:item
+    item ? item.numberOfChildren : 1
+  end
+  
+  def outlineView outlineView, isItemExpandable:item
+    item ? item.numberOfChildren != -1 : true
+  end
+  
+  def outlineView outlineView, child:index, ofItem:item
+    item ? item.childAtIndex(index) : FileSystemItem.rootItem
+  end
+  
+  def outlineView outlineView, objectValueForTableColumn:tableColumn, byItem:item
+    item ? item.relativePath : '/'
+  end
+  
+  def outlineView outlineView, shouldEditTableColumn:tableColumn, item:item
+    false
+  end
+  
+end
