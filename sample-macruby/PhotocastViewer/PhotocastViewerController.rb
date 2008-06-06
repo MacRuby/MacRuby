b0124cc5289d65c148ef5603d71904505bc4e139
Laurent Sansonetti <lsansonetti@apple.com>
2008-06-06 04:19:04 +0000

    new sample, PhotocastViewer, a rewrite of RubyCocoa's RSSPhotoViewer using PubSub
    git-svn-id: http://svn.macosforge.org/repository/ruby/MacRuby/trunk@253 23306eb0-4c56-4727-a40e-e92c0eb68959

---
+++ sample-macruby/PhotocastViewer/PhotocastViewerController.rb
@@ -0,0 +1,80 @@
+class PhotocastViewerController < NSWindowController
+  
+  ib_outlet :imageBrowserView
+  
+  def awakeFromNib
+    @cache = []
+    @imageBrowserView.animates = true
+    @imageBrowserView.dataSource = self
+    @imageBrowserView.delegate = self
+    
+    NSNotificationCenter.defaultCenter.addObserver self,
+      selector:'feedRefreshed:',
+      name:PSFeedRefreshingNotification,
+      object:nil
+  end
+  
+  # Actions
+  
+  def zoomChanged(sender)
+    @imageBrowserView.zoomValue = sender.floatValue
+  end
+  
+  def parse(sender)
+    urlString = sender.stringValue
+    url = NSURL.URLWithString(urlString)
+    feed = PSFeed.alloc.initWithURL(url)
+    feed.refresh(nil)
+  end
+
+  def feedRefreshed(notification)
+    feed = notification.object
+    @results = feed.entryEnumeratorSortedBy(nil).allObjects
+    @cache.clear
+    @imageBrowserView.reloadData
+  end
+  
+  # Image browser datasource/delegate
+
+  def numberOfItemsInImageBrowser(browser)
+    @results ? @results.size : 0
+  end
+  
+  def imageBrowser(browser, itemAtIndex:index)
+    photo = @cache[index]
+    if photo.nil? 
+      entry = @results[index]
+      url = entry.content.HTMLString.scan(/<img\s+src="([^"]+)"/)[0][0]
+      photo = RSSPhoto.new(url)
+      @cache[index] = photo
+    end
+    return photo
+  end
+
+  def imageBrowser_cellWasDoubleClickedAtIndex(browser, index)
+    NSWorkspace.sharedWorkspace.openURL @cache[index].url
+  end
+end
+
+class RSSPhoto
+  attr_reader :url
+  
+  def initialize(url)
+    @urlString = url
+    @url = NSURL.alloc.initWithString url
+  end
+  
+  # IKImageBrowserItem protocol conformance
+  
+  def imageUID
+    @urlString
+  end
+    
+  def imageRepresentationType
+    :IKImageBrowserNSImageRepresentationType
+  end
+  
+  def imageRepresentation    
+    @image ||= NSImage.alloc.initByReferencingURL @url
+  end
+end
---
+++ sample-macruby/PhotocastViewer/main.m
@@ -0,0 +1,14 @@
+//
+//  main.m
+//  PhotocastViewer
+//
+//  Created by Laurent Sansonetti on 6/5/08.
+//  Copyright __MyCompanyName__ 2008. All rights reserved.
+//
+
+#import <MacRuby/MacRuby.h>
+
+int main(int argc, char *argv[])
+{
+    return macruby_main("rb_main.rb", argc, argv);
+}
