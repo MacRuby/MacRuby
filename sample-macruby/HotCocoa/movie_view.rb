aea8763b74d4798ad582ebf21d6fc7d2055bc3fb
Richard Kilmer <rich@infoether.com>
2008-07-08 02:22:47 +0000

    refer to a remote movie
    git-svn-id: http://svn.macosforge.org/repository/ruby/MacRuby/trunk@321 23306eb0-4c56-4727-a40e-e92c0eb68959

--- sample-macruby/HotCocoa/movie_view.rb
+++ sample-macruby/HotCocoa/movie_view.rb
@@ -12,7 +12,7 @@ include HotCocoa
 application do |app|
   window :frame => [100, 100, 500, 500], :title => "HotCocoa!" do |win|
     mview = movie_view :frame => [10, 10, 480, 480], 
-                       :movie => movie(:file => "table_view.mov"),
+                       :movie => movie(:url => "http://www.infoether.com/~rich/hotcocoa.mov"),
                        :controller_buttons => [:back, :volume],
                        :fill_color => color(:name => :black)
     win << mview
