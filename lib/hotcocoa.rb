1f78b1730094bcc9ef7f62acf11ba0eca1b986bb
Richard Kilmer <rich@infoether.com>
2008-07-08 03:35:13 +0000

    move data sources
    git-svn-id: http://svn.macosforge.org/repository/ruby/MacRuby/trunk@325 23306eb0-4c56-4727-a40e-e92c0eb68959

--- lib/hotcocoa.rb
+++ lib/hotcocoa.rb
@@ -19,8 +19,8 @@ require 'hotcocoa/mappings'
 require 'hotcocoa/mapping_methods'
 require 'hotcocoa/mapper'
 require 'hotcocoa/delegate_builder'
-require 'hotcocoa/table_data_source'
-require 'hotcocoa/combo_box_data_source'
+require 'hotcocoa/data_sources/table_data_source'
+require 'hotcocoa/data_sources/combo_box_data_source'
 require 'hotcocoa/kernel_ext'
 
 HotCocoa::Mappings.reload
