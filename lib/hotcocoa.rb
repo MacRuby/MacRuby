b498fc9c1485d999905f41ed6faa0534b3b8d1e4
Richard Kilmer <rich@infoether.com>
2008-07-09 22:15:16 +0000

    refactoring layout view
    git-svn-id: http://svn.macosforge.org/repository/ruby/MacRuby/trunk@346 23306eb0-4c56-4727-a40e-e92c0eb68959

--- lib/hotcocoa.rb
+++ lib/hotcocoa.rb
@@ -9,6 +9,7 @@ end
 require 'hotcocoa/mappings'
 require 'hotcocoa/mapping_methods'
 require 'hotcocoa/mapper'
+require 'hotcocoa/layout_view'
 require 'hotcocoa/delegate_builder'
 require 'hotcocoa/data_sources/table_data_source'
 require 'hotcocoa/data_sources/combo_box_data_source'
