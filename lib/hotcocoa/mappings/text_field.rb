3db1d219b38dabde83b7f7b82ff1457175cbf77a
Richard Kilmer <rich@infoether.com>
2008-08-03 04:44:50 +0000

    add text align
    git-svn-id: http://svn.macosforge.org/repository/ruby/MacRuby/trunk@395 23306eb0-4c56-4727-a40e-e92c0eb68959

--- lib/hotcocoa/mappings/text_field.rb
+++ lib/hotcocoa/mappings/text_field.rb
@@ -1,9 +1,23 @@
 HotCocoa::Mappings.map :text_field => :NSTextField do
+
+  constant :text_align, {
+    :right  => NSRightTextAlignment,
+    :left   => NSLeftTextAlignment,
+    :center => NSCenterTextAlignment
+  }
   
-  defaults :selectable => true, :editable => true, :layout => {}
+  defaults :selectable => true, :editable => true, :layout => {}, :frame => DefaultEmptyRect
   
   def init_with_options(text_field, options)
     text_field.initWithFrame options.delete(:frame)
   end
   
+  custom_methods do
+
+    def text_align=(value)
+      setAlignment(value)
+    end
+
+  end
+
 end
