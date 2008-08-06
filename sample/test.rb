a407e79d9687ee9199dd8744f0d5ca1fe1f87547
Laurent Sansonetti <lsansonetti@apple.com>
2008-08-06 09:29:17 +0000

    Object is now NSObject
    git-svn-id: http://svn.macosforge.org/repository/ruby/MacRuby/branches/lrz_unstable@412 23306eb0-4c56-4727-a40e-e92c0eb68959

--- sample/test.rb
+++ sample/test.rb
@@ -1910,8 +1910,8 @@ STDERR.print "intentionally redefines TEST3, TEST4\n" if $VERBOSE
 test_ok([TEST1,TEST2,TEST3,TEST4] == [1,2,6,8])
 
 
-test_ok((String <=> NSObject) == -1)
-test_ok((NSObject <=> String) == 1)
+test_ok((String <=> Object) == -1)
+test_ok((Object <=> String) == 1)
 test_ok((Array <=> String) == nil)
 
 test_check "clone"
