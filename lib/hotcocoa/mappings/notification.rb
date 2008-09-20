33443fc3dd2624bc6573b8181bb46f741e7ce842
Laurent Sansonetti <lsansonetti@apple.com>
2008-09-20 00:08:16 +0000

    fix typos
    git-svn-id: http://svn.macosforge.org/repository/ruby/MacRuby/trunk@609 23306eb0-4c56-4727-a40e-e92c0eb68959

--- lib/hotcocoa/mappings/notification.rb
+++ lib/hotcocoa/mappings/notification.rb
@@ -5,12 +5,12 @@ HotCocoa::Mappings.map :notification => :NSNotification do
   def alloc_with_options(options)
     if options.delete(:post)
       if options.delete(:distributed)
-        NSDistributedNotificationCenter.defaultCenter.postNotificationWithName options.delete(:name), object:options.delete(:object), userInfo:options.delete(:info), deliverImmediately:(options.delete(:immediately) == true)
+        NSDistributedNotificationCenter.defaultCenter.postNotificationName options.delete(:name), object:options.delete(:object), userInfo:options.delete(:info), deliverImmediately:(options.delete(:immediately) == true)
       else
-        NSNotificationCenter.defaultCenter.postNotificationWithName options.delete(:name), object:options.delete(:object), userInfo:options.delete(:info)
+        NSNotificationCenter.defaultCenter.postNotificationName options.delete(:name), object:options.delete(:object), userInfo:options.delete(:info)
       end
     else
-      NSNotification.notificationWithName options.delete(:name), object:options.delete(:object), userInfo:options.delete(:info)
+      NSNotification.notificationName options.delete(:name), object:options.delete(:object), userInfo:options.delete(:info)
     end
   end
   
