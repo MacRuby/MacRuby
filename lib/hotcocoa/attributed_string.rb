ae241bdb25c469d7f76e4d8d8d7efee1a5cad848
Benjamin Stiglitz <ben@tanjero.com>
2008-11-21 04:15:20 +0000

    Lots of changes on attributed strings
    git-svn-id: http://svn.macosforge.org/repository/ruby/MacRuby/trunk@749 23306eb0-4c56-4727-a40e-e92c0eb68959

--- lib/hotcocoa/attributed_string.rb
+++ lib/hotcocoa/attributed_string.rb
@@ -1,14 +1,43 @@
 module HotCocoa
     class NSRangedProxyAttributeHash
+	ATTRIBUTE_KEYS = { :font => NSFontAttributeName,
+	                   :paragraph_style => NSParagraphStyleAttributeName,
+                           :color => NSForegroundColorAttributeName,
+			   :underline_style => NSUnderlineStyleAttributeName,
+			   :superscript => NSSuperscriptAttributeName,
+			   :background_color => NSBackgroundColorAttributeName,
+			   :attachment => NSAttachmentAttributeName,
+			   :ligature => NSLigatureAttributeName,
+			   :baseline_offset => NSBaselineOffsetAttributeName,
+			   :kerning => NSKernAttributeName,
+			   :link => NSLinkAttributeName,
+			   :stroke_width => NSStrokeWidthAttributeName,
+			   :stroke_color => NSStrokeColorAttributeName,
+			   :underline_color => NSUnderlineColorAttributeName,
+			   :strikethrough_style => NSStrikethroughStyleAttributeName,
+			   :strikethrough_color => NSStrikethroughColorAttributeName,
+			   :shadow => NSShadowAttributeName,
+			   :obliqueness => NSObliquenessAttributeName,
+			   :expansion_factor => NSExpansionAttributeName,
+			   :cursor => NSCursorAttributeName,
+			   :tool_tip => NSToolTipAttributeName,
+			   :character_shape => NSCharacterShapeAttributeName,
+			   :glyph_info => NSGlyphInfoAttributeName,
+			   :marked_clause_segment => NSMarkedClauseSegmentAttributeName,
+			   :spelling_state => NSSpellingStateAttributeName }
+
+
 	def initialize(proxy)
 	    @proxy = proxy
 	end
 
 	def [](k)
+	    k = attribute_for_key(k)
 	    @proxy.string.attribute(k, atIndex:@proxy.range.first, effectiveRange:nil)
 	end
 
 	def []=(k,v)
+	    k = attribute_for_key(k)
 	    @proxy.string.removeAttribute(k, range:@proxy.range.to_NSRange(@proxy.string.length - 1))
 	    @proxy.string.addAttribute(k, value:v, range:@proxy.range.to_NSRange(@proxy.string.length - 1))
 	end
@@ -17,8 +46,29 @@ module HotCocoa
 	    attributes.each_pair do |k, v|
 		self[k] = v
 	    end
+	    self
 	end
 	alias :merge :<<
+
+	def to_hash
+	    @proxy.string.attributesAtIndex(@proxy.range.first, effectiveRange:nil).inject({}) do |h, pair|
+		h[key_for_attribute(pair.first)] = pair.last
+		h
+	    end
+	end
+
+	def inspect
+	    to_hash.inspect
+	end
+
+	private
+	def key_for_attribute(attribute)
+	    (ATTRIBUTE_KEYS.select { |k,v| v == attribute }.first || [attribute]).first
+	end
+
+	def attribute_for_key(key)
+	    ATTRIBUTE_KEYS[key] || key
+	end
     end
 
     class NSRangedProxyAttributedString
@@ -36,16 +86,21 @@ end
 
 class String
     def with_attributes(attributes = {})
-	NSMutableAttributedString.alloc.initWithString(self, :attributes => attributes)
+	attributed_string = NSMutableAttributedString.alloc.initWithString(self)
+	attributed_string.attributes << attributes
+	attributed_string
     end
 end
 
 class Range
     def to_NSRange(max = nil)
+	location = first
 	if last == -1 and max
-	    last = max
+	    length = max - first + 1
+	else
+	    length = last - first + 1
 	end
-	NSRange.new(first, last - first + 1)
+	NSRange.new(location, length)
     end
 end
 
@@ -61,6 +116,12 @@ class NSMutableAttributedString
 	else
 	    appendAttributedString s
 	end
+    end
+
+    def +(s)
+	attributed_string = mutableCopy
+	attributed_string << s
+	attributed_string
     end
 
     def attributes
