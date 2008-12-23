f7cc44504a7cdc7fdced1c4acde6c483a8293764
Eloy Duran <eloy.de.enige@gmail.com>
2008-12-23 15:45:53 +0000

    Added bug test case which demonstrates a endless loop when using the private class method on a Kernel method.
    git-svn-id: http://svn.macosforge.org/repository/ruby/MacRuby/trunk@769 23306eb0-4c56-4727-a40e-e92c0eb68959

--- test-macruby/known_bugs.rb
+++ test-macruby/known_bugs.rb
@@ -12,4 +12,24 @@ module KnownBugs
       assert_nothing_raised { YAML.load(data) }
     end
   end
+  
+  class TestKernel < Test::Unit::TestCase
+    module ::Kernel
+      private
+      def is_callable?; true end
+    end
+    
+    module ::Kernel
+      def should_be_callable?; true end
+      private :should_be_callable?
+    end
+    
+    def test_kernel_methods_made_private_with_keyword
+      assert is_callable? # works
+    end
+    
+    def test_kernel_methods_made_private_with_class_method
+      assert should_be_callable? # causes endless loop
+    end
+  end
 end
