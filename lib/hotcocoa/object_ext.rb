162c3d6aa9d6169ca8dbc4a652e00ae83d3a6a17
Eloy Duran <eloy.de.enige@gmail.com>
2009-01-22 16:30:50 +0000

    Added tests for hotcocoa/object_ext.rb from upstream and missing ones. Also added poper copyright and license attribution to Sam Smoot.
    git-svn-id: http://svn.macosforge.org/repository/ruby/MacRuby/trunk@807 23306eb0-4c56-4727-a40e-e92c0eb68959

--- lib/hotcocoa/object_ext.rb
+++ lib/hotcocoa/object_ext.rb
@@ -1,5 +1,7 @@
+# Object.full_const_get was taken from the ‘extlib’ project:
+# http://github.com/sam/extlib which is released under a MIT License and
+# copyrighted by Sam Smoot (2008).
 
-# This is ripped from sam/extlib and changed a little bit.
 class Object
 
   # @param name<String> The name of the constant to get, e.g. "Merb::Router".
@@ -7,13 +9,12 @@ class Object
   # @return <Object> The constant corresponding to the name.
   def full_const_get(name)
     list = name.split("::")
-    list.shift if list.first.strip.empty?
+    list.shift if list.first.empty?
     obj = self
     list.each do |x|
       # This is required because const_get tries to look for constants in the
       # ancestor chain, but we only want constants that are HERE
-      raise NameError, "uninitialized constant #{self.name}::#{name}" unless obj.const_defined?(x)
-      obj = obj.const_get(x)
+      obj = obj.const_defined?(x) ? obj.const_get(x) : obj.const_missing(x)
     end
     obj
   end
