fe17235a713c41fa2a828edb8286eda25efbb0f6
Eloy Duran <eloy.de.enige@gmail.com>
2009-03-12 00:03:19 +0000

    Fixed previous tests and added another one _with_ required argument.
    git-svn-id: http://svn.macosforge.org/repository/ruby/MacRuby/branches/experimental@880 23306eb0-4c56-4727-a40e-e92c0eb68959

--- test_roxor.rb
+++ test_roxor.rb
@@ -572,9 +572,10 @@ test "dispatch" do
   assert "42", "def foo(x,y,z); x+y+z; end; a=[20]; b=[10]; c=[12]; p foo(*a, *b, *c)"
   assert "42", "def foo(x); x; end; a=42; p foo(*a)"
 
-  assert ":ok", "def foo(*args); :ok; end; foo"
-  assert ":ok", "def foo(&block); :ok; end; foo"
-  assert ":ok", "def foo(*args, &block); :ok; end; foo"
+  assert ":ok", "def foo(*args); :ok; end; p foo"
+  assert ":ok", "def foo(&block); :ok; end; p foo"
+  assert ":ok", "def foo(*args, &block); :ok; end; p foo"
+  assert ":ok", "def foo(x, *args, &block); x; end; p foo(:ok)"
 end
 
 test "blocks" do
