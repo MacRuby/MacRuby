9b774cb671183d7ebd3a1b644914d7fd3c99ff23
Eloy Duran <eloy.de.enige@gmail.com>
2009-03-23 15:02:10 +0000

    Fixed description of example, on 1.9 retry is not supported outside a block.
    git-svn-id: http://svn.macosforge.org/repository/ruby/MacRuby/branches/experimental@1108 23306eb0-4c56-4727-a40e-e92c0eb68959

--- spec/frozen/language/versions/redo_1.9.rb
+++ spec/frozen/language/versions/redo_1.9.rb
@@ -1,5 +1,5 @@
 describe "The redo statement" do
-  it "raises a SyntaxError if used not within block or while/for loop" do
+  it "raises a SyntaxError if used not within block" do
     lambda do
       instance_eval "def bad_meth_redo; redo; end"
     end.should raise_error(SyntaxError)
