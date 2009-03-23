63d0fada89ce41100f8a293795d0dbfa18a9ebde
Eloy Duran <eloy.de.enige@gmail.com>
2009-03-23 00:04:02 +0000

    Moved block argument specs into version specific files and added Ruby 1.9 behaviour examples.
    git-svn-id: http://svn.macosforge.org/repository/ruby/MacRuby/branches/experimental@1070 23306eb0-4c56-4727-a40e-e92c0eb68959

---
+++ spec/frozen/language/versions/block_1.9.rb
@@ -0,0 +1,31 @@
+describe "Block parameters" do
+  it "does not override a shadowed variable from the outer scope" do
+    i = 0
+    a = [1,2,3]
+    a.each {|i| ;}
+    i.should == 0
+  end
+
+  it "captures variables from the outer scope" do
+    a = [1,2,3]
+    sum = 0
+    var = nil
+    a.each {|var| sum += var}
+    sum.should == 6
+    var.should == nil
+  end
+end
+
+describe "Block parameters (to be removed from MRI)" do
+  it "raises a SyntaxError when using a global variable" do
+    lambda do
+      eval "[].each {|$global_for_block_assignment| ;}"
+    end.should raise_error(SyntaxError)
+  end
+  
+  it "raises a SyntaxError when making a method call" do
+    lambda do
+      eval "o = Object.new; [].each {|o.id| }"
+    end.should raise_error(SyntaxError)
+  end
+end
