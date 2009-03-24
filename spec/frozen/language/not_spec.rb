31aebb3f4cd93d983c36f64dc9c1589356a35144
Eloy Duran <eloy.de.enige@gmail.com>
2009-03-24 09:59:03 +0000

    Added a few specs for the "not" and the "!" keywords.
    git-svn-id: http://svn.macosforge.org/repository/ruby/MacRuby/branches/experimental@1126 23306eb0-4c56-4727-a40e-e92c0eb68959

--- spec/frozen/language/not_spec.rb
+++ spec/frozen/language/not_spec.rb
@@ -1,1 +1,25 @@
 require File.dirname(__FILE__) + '/../spec_helper'
+
+describe "The not keyword" do
+  it "negates a `true' value" do
+    (not true).should be_false
+    (not 'true').should be_false
+  end
+
+  it "negates a `false' value" do
+    (not false).should be_true
+    (not nil).should be_true
+  end
+end
+
+describe "The `!' keyword" do
+  it "negates a `true' value" do
+    (!true).should be_false
+    (!'true').should be_false
+  end
+
+  it "negates a `false' value" do
+    (!false).should be_true
+    (!nil).should be_true
+  end
+end
