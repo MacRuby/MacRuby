a8750de75aff669a616f3edeea691bc18f1e4f23
Laurent Sansonetti <lsansonetti@apple.com>
2009-04-03 05:31:02 +0000

    uncommented some code that now compiles (and runs, yay)
    git-svn-id: http://svn.macosforge.org/repository/ruby/MacRuby/branches/experimental@1295 23306eb0-4c56-4727-a40e-e92c0eb68959

--- spec/frozen/language/for_spec.rb
+++ spec/frozen/language/for_spec.rb
@@ -49,16 +49,15 @@ describe "The for expression" do
     n.should == 3
   end
   
-  # TODO: commented out due to a compiler error
-  #it "allows a class variable as an iterator name" do
-  #  m = [1,2,3]
-  #  n = 0
-  #  for @@var in m
-  #    n += 1
-  #  end
-  #  @@var.should == 3
-  #  n.should == 3
-  #end
+  it "allows a class variable as an iterator name" do
+    m = [1,2,3]
+    n = 0
+    for @@var in m
+      n += 1
+    end
+    @@var.should == 3
+    n.should == 3
+  end
 
   it "splats multiple arguments together if there are fewer arguments than values" do
     class OFor
