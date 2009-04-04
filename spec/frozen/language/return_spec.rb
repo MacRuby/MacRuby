f931bba0b18beff4946eac75fb3e98e9c61e5f6f
Laurent Sansonetti <lsansonetti@apple.com>
2009-04-04 00:14:38 +0000

    converted to 1.9 behaviors
    git-svn-id: http://svn.macosforge.org/repository/ruby/MacRuby/branches/experimental@1313 23306eb0-4c56-4727-a40e-e92c0eb68959

--- spec/frozen/language/return_spec.rb
+++ spec/frozen/language/return_spec.rb
@@ -23,21 +23,47 @@ describe "The return keyword" do
 
 
   describe "in a Thread" do
-    it "raises a ThreadError if used to exit a thread" do
-      lambda { Thread.new { return }.join }.should raise_error(ThreadError)
+    ruby_version_is "" ... "1.9" do
+      it "raises a ThreadError if used to exit a thread" do
+        lambda { Thread.new { return }.join }.should raise_error(ThreadError)
+      end
+    end
+
+    ruby_version_is "1.9" do
+      it "raises a LocalJumpError if used to exit a thread" do
+        lambda { Thread.new { return }.join }.should raise_error(LocalJumpError)
+      end
     end
   end
 
 
   describe "when passed a splat" do
-    it "returns nil when the ary is empty" do
-      def r; ary = []; return *ary; end
-      r.should be_nil
+    ruby_version_is "" ... "1.9" do
+      it "returns nil when the ary is empty" do
+        def r; ary = []; return *ary; end
+        r.should be_nil
+      end
     end
 
-    it "returns the first element when the array is size of 1" do
-      def r; ary = [1]; return *ary; end
-      r.should == 1
+    ruby_version_is "1.9" do
+      it "returns [] when the ary is empty" do
+        def r; ary = []; return *ary; end
+        r.should == []
+      end
+    end
+
+    ruby_version_is "" ... "1.9" do
+      it "returns the first element when the array is size of 1" do
+        def r; ary = [1]; return *ary; end
+        r.should == 1
+      end
+    end
+
+    ruby_version_is "1.9" do
+      it "returns the array when the array is size of 1" do
+        def r; ary = [1]; return *ary; end
+        r.should == [1]
+      end
     end
 
     it "returns the whole array when size is greater than 1" do
@@ -48,9 +74,18 @@ describe "The return keyword" do
       r.should == [1,2,3]
     end
 
-    it "returns a non-array when used as a splat" do
-      def r; value = 1; return *value; end
-      r.should == 1
+    ruby_version_is "" ... "1.9" do
+      it "returns a non-array when used as a splat" do
+        def r; value = 1; return *value; end
+        r.should == 1
+      end
+    end
+
+    ruby_version_is "1.9" do
+      it "returns an array when used as a splat" do
+        def r; value = 1; return *value; end
+        r.should == [1]
+      end
     end
 
     it "calls 'to_a' on the splatted value first" do
@@ -63,7 +98,13 @@ describe "The return keyword" do
         return *obj
       end
 
-      r().should be_nil
+      ruby_version_is "" ... "1.9" do
+        r().should be_nil
+      end
+
+      ruby_version_is "1.9" do
+        r().should == []
+      end
 
       def r
         obj = Object.new
@@ -74,7 +115,13 @@ describe "The return keyword" do
         return *obj
       end
 
-      r().should == 1
+      ruby_version_is "" ... "1.9" do
+        r().should == 1
+      end
+
+      ruby_version_is "1.9" do
+        r().should == [1]
+      end
 
       def r
         obj = Object.new
@@ -88,39 +135,41 @@ describe "The return keyword" do
       r().should == [1,2]
     end
 
-    it "calls 'to_ary' on the splatted value first" do
-      def r
-        obj = Object.new
-        def obj.to_ary
-          []
+    ruby_version_is "" ... "1.9" do
+      it "calls 'to_ary' on the splatted value first" do
+        def r
+          obj = Object.new
+          def obj.to_ary
+            []
+          end
+  
+          return *obj
         end
-
-        return *obj
-      end
-
-      r().should be_nil
-
-      def r
-        obj = Object.new
-        def obj.to_ary
-          [1]
+  
+        r().should be_nil
+  
+        def r
+          obj = Object.new
+          def obj.to_ary
+            [1]
+          end
+  
+          return *obj
         end
-
-        return *obj
-      end
-
-      r().should == 1
-
-      def r
-        obj = Object.new
-        def obj.to_ary
-          [1,2]
+  
+        r().should == 1
+  
+        def r
+          obj = Object.new
+          def obj.to_ary
+            [1,2]
+          end
+  
+          return *obj
         end
-
-        return *obj
+  
+        r().should == [1,2]
       end
-
-      r().should == [1,2]
     end
   end
 
@@ -186,9 +235,11 @@ describe "The return keyword" do
 
 
   describe "within a block" do
-    it "raises a LocalJumpError if there is no lexicaly enclosing method" do
-      def f; yield; end
-      lambda { f { return 5 } }.should raise_error(LocalJumpError)
+    ruby_version_is "" ... "1.9" do
+      it "raises a LocalJumpError if there is no lexicaly enclosing method" do
+        def f; yield; end
+        lambda { f { return 5 } }.should raise_error(LocalJumpError)
+      end
     end
 
     it "causes lambda to return nil if invoked without any arguments" do
