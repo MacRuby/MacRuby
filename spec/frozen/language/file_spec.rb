44169de46cf90cd61e455a4fb75f1bfa8f85e7d2
Eloy Duran <eloy.de.enige@gmail.com>
2009-04-22 22:07:50 +0000

    Add expectation for directory part of __FILE__
    git-svn-id: http://svn.macosforge.org/repository/ruby/MacRuby/branches/experimental@1443 23306eb0-4c56-4727-a40e-e92c0eb68959

--- spec/frozen/language/file_spec.rb
+++ spec/frozen/language/file_spec.rb
@@ -6,6 +6,16 @@ describe "The __FILE__ constant" do
   it "equals the current filename" do
     File.basename(__FILE__).should == "file_spec.rb"
   end
+  
+  it "equals the full path to the file when required" do
+    $:.unshift File.dirname(__FILE__) + '/fixtures'
+    begin
+      require 'file.rb'
+      ScratchPad.recorded.should == File.dirname(__FILE__) + '/fixtures/file.rb'
+    ensure
+      $:.shift
+    end
+  end
 
   it "equals (eval) inside an eval" do
     eval("__FILE__").should == "(eval)"
