0199e3b0544a4c1690d9ec6ced515255749b69b5
Eloy Duran <eloy.de.enige@gmail.com>
2009-04-22 22:11:14 +0000

    Correcting style in last commit:
    * No need for 2 arrays in the first test case.
    * Adding guards for versions < 1.9
    * Fixing descriptions (we don't use should).
    
    
    git-svn-id: http://svn.macosforge.org/repository/ruby/MacRuby/branches/experimental@1462 23306eb0-4c56-4727-a40e-e92c0eb68959

--- spec/frozen/core/array/sample_spec.rb
+++ spec/frozen/core/array/sample_spec.rb
@@ -1,20 +1,23 @@
 require File.dirname(__FILE__) + '/../../spec_helper'
 require File.dirname(__FILE__) + '/fixtures/classes'
 
-
 describe "Array#sample" do
+  ruby_version_is "" ... "1.9" do
+    it "is not defined" do
+      lambda { [].sample }.should raise_error(NoMethodError)
+    end
+  end
+
   ruby_version_is "1.9" do
-    it "should select an value from the array" do
+    it "selects a random value from the array" do
       a = [1,2,3,4]
-      b = [4,3,2,1]
       10.times {
-        b.include?(a.sample).should be_true
+        a.include?(a.sample).should be_true
       }
     end
 
-    it "should return a nil value if the array is empty" do
-      a = []
-      a.sample.should be_nil
+    it "returns nil for empty arrays" do
+      [].sample.should be_nil
     end
   end
 end
