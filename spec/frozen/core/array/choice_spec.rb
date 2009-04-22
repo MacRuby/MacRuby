4312dfc80e38e4323c13953785c634fd790eee22
Eloy Duran <eloy.de.enige@gmail.com>
2009-04-22 22:10:55 +0000

    Correcting last patch.
    * Array#choice was introduced in Ruby 1.8.7, adding guard for 1.8.6
    * Fixing ranges
    * Fixing descriptions.
    
    
    git-svn-id: http://svn.macosforge.org/repository/ruby/MacRuby/branches/experimental@1460 23306eb0-4c56-4727-a40e-e92c0eb68959

--- spec/frozen/core/array/choice_spec.rb
+++ spec/frozen/core/array/choice_spec.rb
@@ -2,25 +2,28 @@ require File.dirname(__FILE__) + '/../../spec_helper'
 require File.dirname(__FILE__) + '/fixtures/classes'
 
 describe "Array#choice" do
-  ruby_version_is "" .. "1.9" do
-    it "should select an value from the array" do
+  ruby_version_is "" ... "1.8.7" do
+    it "raises NoMethodError" do
+      lambda { [].choice }.should raise_error(NoMethodError)
+    end
+  end
+
+  ruby_version_is "1.8.7" ... "1.9" do
+    it "selects a random value from the array" do
       a = [1,2,3,4]
-      b = [4,3,2,1]
       10.times {
-        b.include?(a.choice).should be_true
+        a.include?(a.choice).should be_true
       }
     end
 
-    it "should return a nil value if the array is empty" do
-      a = []
-      a.choice.should be_nil
+    it "returns nil for empty arrays" do
+      [].choice.should be_nil
     end
   end
 
   ruby_version_is "1.9" do
-    it "should raise NoMethodError in ruby 1.9" do
-      a = [1,2,3,4]
-      lambda { a.choice }.should raise_error(NoMethodError)
+    it "raises NoMethodError" do
+      lambda { [].choice }.should raise_error(NoMethodError)
     end
   end
 end
