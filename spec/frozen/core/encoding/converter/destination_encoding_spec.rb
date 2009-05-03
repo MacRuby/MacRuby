de0d511180ee4191904ddbfebc76f7a4f1ce2155
Eloy Duran <eloy.de.enige@gmail.com>
2009-05-03 13:02:58 +0000

    Created spec stubs for the Encoding related APIs
    git-svn-id: http://svn.macosforge.org/repository/ruby/MacRuby/branches/experimental@1521 23306eb0-4c56-4727-a40e-e92c0eb68959

---
+++ spec/frozen/core/encoding/converter/destination_encoding_spec.rb
@@ -0,0 +1,5 @@
+require File.dirname(__FILE__) + '/../../../spec_helper'
+
+describe "Encoding::Converter#destination_encoding" do
+  it "needs to be reviewed for spec completeness"
+end
---
+++ spec/frozen/core/encoding/converter/last_error_spec.rb
@@ -0,0 +1,5 @@
+require File.dirname(__FILE__) + '/../../../spec_helper'
+
+describe "Encoding::Converter#last_error" do
+  it "needs to be reviewed for spec completeness"
+end
