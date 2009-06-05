f8c0e2c0cc366dce6ac4b00b190e3793c6263b49
Laurent Sansonetti <lsansonetti@apple.com>
2009-06-05 01:57:07 +0000

    adding/fixing tests
    git-svn-id: http://svn.macosforge.org/repository/ruby/MacRuby/branches/experimental@1743 23306eb0-4c56-4727-a40e-e92c0eb68959

--- test_vm/require.rb
+++ test_vm/require.rb
@@ -6,8 +6,11 @@ assert ":ok", %q{
   end
 }
 
+assert ":ok", "require '#{Dir.pwd + '/fixtures/foo'}'"
+assert ":ok", "require '#{Dir.pwd + '/fixtures/foo.rb'}'"
+
 assert ":ok", "$:.unshift('fixtures'); require 'foo'"
 
 assert ":ok", "begin; require 'fixtures/raise'; rescue NameError; p :ok; end"
 
-assert ":ok", "begin; require '/doesnotexist'; rescue NameError; p :ok; end"
+assert ":ok", "begin; require '/doesnotexist'; rescue LoadError; p :ok; end"
