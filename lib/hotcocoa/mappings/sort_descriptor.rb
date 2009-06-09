5b476fa038d7440100799b6b9c2192f6276ecb36
Richard Kilmer <rich@infoether.com>
2009-06-09 06:51:06 +0000

    added sort descriptor
    git-svn-id: http://svn.macosforge.org/repository/ruby/MacRuby/trunk@1828 23306eb0-4c56-4727-a40e-e92c0eb68959

--- lib/hotcocoa/mappings/sort_descriptor.rb
+++ lib/hotcocoa/mappings/sort_descriptor.rb
@@ -2,11 +2,11 @@ HotCocoa::Mappings.map :sort_descriptor => :NSSortDescriptor do
 
   defaults :ascending => true
 
-  def init_with_options(sd, opts)
+  def init_with_options(sort_descriptor, opts)
     if opts.has_key?(:selector)
-      sd.initWithKey(opts.delete(:key), ascending:opts.delete(:ascending))
+      sort_descriptor.initWithKey(opts.delete(:key), ascending:opts.delete(:ascending))
     else
-      sd.initWithKey(opts.delete(:key), ascending:opts.delete(:ascending), selector:opts.delete(:selector))
+      sort_descriptor.initWithKey(opts.delete(:key), ascending:opts.delete(:ascending), selector:opts.delete(:selector))
     end
   end
 
