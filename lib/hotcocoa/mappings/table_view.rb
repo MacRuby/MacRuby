b3b0d25737221f563c8463adba7c752d071a777b
Richard Kilmer <rich@infoether.com>
2009-06-09 06:37:51 +0000

    add delegate methods and selection_style constant map  per #268
    git-svn-id: http://svn.macosforge.org/repository/ruby/MacRuby/trunk@1824 23306eb0-4c56-4727-a40e-e92c0eb68959

--- lib/hotcocoa/mappings/table_view.rb
+++ lib/hotcocoa/mappings/table_view.rb
@@ -18,6 +18,11 @@ HotCocoa::Mappings.map :table_view => :NSTableView do
   	:both               => NSTableViewSolidVerticalGridLineMask | NSTableViewSolidHorizontalGridLineMask 
   }
 
+  constant :selection_style, {
+    :regular            => NSTableViewSelectionHighlightStyleRegular,
+    :source_list        => NSTableViewSelectionHighlightStyleSourceList
+  }
+
   def init_with_options(table_view, options)
     table_view.initWithFrame(options.delete(:frame))
   end
@@ -55,10 +60,6 @@ HotCocoa::Mappings.map :table_view => :NSTableView do
    	  setGridStyleMask(value) 
    	end
 
-    def row_height=(value) 
-      setRowHeight(value) 
-    end
-
     def on_double_action=(behavior)
       if target && (
         target.instance_variable_get("@action_behavior") || 
@@ -82,5 +83,28 @@ HotCocoa::Mappings.map :table_view => :NSTableView do
     end
 
   end
+
+  delegating "tableView:willDisplayCell:forTableColumn:row:",               :to => :will_display_cell,      :parameters => [:willDisplayCell, :forTableColumn, :row]
+  delegating "tableView:dataCellForTableColumn:row:",                       :to => :data_cell,              :parameters => [:dataCellForTableColumn, :row]
+  delegating "tableView:shouldShowCellExpansionForTableColumn:row:",        :to => :expand_cell?,           :parameters => [:shouldShowCellExpansionForTableColumn, :row]
+  delegating "tableView:isGroupRow:",                                       :to => :is_group_row?,          :parameters => [:isGroupRow]
+  delegating "tableView:shouldEditTableColumn:row:",                        :to => :edit_table_column?,     :parameters => [:shouldEditTableColumn, :row]
+  delegating "tableView:heightOfRow:",                                      :to => :height_of_row,          :parameters => [:heightOfRow]
+  delegating "selectionShouldChangeInTableView:",                           :to => :change_selection?
+  delegating "tableView:shouldSelectRow:",                                  :to => :select_row?,            :parameters => [:shouldSelectRow]
+  delegating "tableView:selectionIndexesForProposedSelection:",             :to => :indexes_for_selection,  :parameters => [:selectIndexesForProposedSelection]
+  delegating "tableView:shouldSelectTableColumn:",                          :to => :select_column?,         :parameters => [:shouldSelectTableColumn]
+  delegating "tableViewSelectionIsChanging:",                               :to => :selection_changing,     :parameters => [:tableViewSelectionIsChanging]
+  delegating "tableViewSelectionDidChange:",                                :to => :selection_changed,      :parameters => [:tableViewSelectionDidChange]
+  delegating "tableView:shouldTypeSelectForEvent:withCurrentSearchString:", :to => :type_select_for_event?, :parameters => [:shouldTypeSelectForEvent, :withCurrentSearchString]
+  delegating "tableView:typeSelectStringForTableColumn:row:",               :to => :type_select_string,     :parameters => [:typeSelectStringForTableColumn, :row]
+  delegating "tableView:nextTypeSelectMatchFromRow:toRow:forString:",       :to => :find_in_range,          :parameters => [:nextTypeSelectMatchFromRow, :toRow, :forString]
+  delegating "tableView:didDragTableColumn:",                               :to => :dragged_column,         :parameters => [:didDragTableColumn]
+  delegating "tableViewColumnDidMove:",                                     :to => :column_moved,           :parameters => [:tableViewColumnDidMove]
+  delegating "tableViewColumnDidResize:",                                   :to => :column_resized,         :parameters => [:tableViewColumnDidResize]
+  delegating "tableView:didClickTableColumn:",                              :to => :clicked_column,         :parameters => [:didClickTableColumn]
+  delegating "tableView:mouseDownInHeaderOfTableColumn:",                   :to => :header_clicked,         :parameters => [:mouseDownInHeaderOfTableColumn]
+  delegating "tableView:shouldTrackCell:forTableColumn:row:",               :to => :track_cell?,            :parameters => [:shouldTrackCell, :forTableColumn, :row]
+  delegating "tableView:toolTipForCell:rect:tableColumn:row:mouseLocation:",:to => :tooltip_for_cell,       :parameters => [:toolTipForCell, :rect, :tableColumn, :row, :mouseLocation]
 
 end
