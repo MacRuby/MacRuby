312f8fa6cbfbacbee874729043e06471262551f4
Laurent Sansonetti <lsansonetti@apple.com>
2009-07-24 05:18:17 +0000

    adding one more failing test
    git-svn-id: http://svn.macosforge.org/repository/ruby/MacRuby/branches/experimental@2067 23306eb0-4c56-4727-a40e-e92c0eb68959

--- test_vm/block.rb
+++ test_vm/block.rb
@@ -182,6 +182,7 @@ assert "42", "p proc { next 42 }.call"
 assert "42", "begin p proc { break 24 }.call rescue LocalJumpError; p 42 end"
 assert "42", "def foo; yield; end; foo { break }; 1.times {p 42}"
 assert "42", "1.times { begin; break; ensure; p 42; end }"
+assert "42", "i=0; while i<1000; begin; break; ensure; p 42; end; end"
 
 assert "42", "p [42].map { |x| x }.map { |y| y }[0]"
 
