d7ff013bd39442b7db00c563ed40ee57119b2ed7
Eloy Duran <eloy.de.enige@gmail.com>
2009-08-04 08:01:44 +0000

    Gets the NSNumber boolean conversion spec working. Patch by Dan Sinclair.
    git-svn-id: http://svn.macosforge.org/repository/ruby/MacRuby/branches/experimental@2207 23306eb0-4c56-4727-a40e-e92c0eb68959

--- spec/macruby/core/number_spec.rb
+++ spec/macruby/core/number_spec.rb
@@ -3,9 +3,9 @@ require File.dirname(__FILE__) + "/../spec_helper"
 describe "An NSNumber boolean object" do
   it "can be compared against a true/false Ruby type" do
     true.should == NSNumber.numberWithBool(true)
-    true.should != NSNumber.numberWithBool(false)
+    true.should_not == NSNumber.numberWithBool(false)
     false.should == NSNumber.numberWithBool(false)
-    false.should != NSNumber.numberWithBool(true)
+    false.should_not == NSNumber.numberWithBool(true)
   end
 end
 
