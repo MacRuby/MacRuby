e2d15913631d05ef253777d8a9530401b2e9da66
Matt Aimonetti <mattaimonetti@gmail.com>
2009-08-17 05:25:57 +0000

    added a test_vm assertion for begin/raise/else
    git-svn-id: http://svn.macosforge.org/repository/ruby/MacRuby/trunk@2342 23306eb0-4c56-4727-a40e-e92c0eb68959

--- test_vm/exception.rb
+++ test_vm/exception.rb
@@ -8,6 +8,8 @@ assert ":ok", %q{
 assert "42", "x = 40; begin; x += 1; rescue; ensure; x += 1; end; p x"
 assert "42", "x = 40; begin; raise; x = nil; rescue; x += 1; ensure; x += 1; end; p x"
 
+assert "42", "x = 40; begin; x; rescue => e; else; x = 42 ; end; p x"
+
 assert "42", "x = begin; 42; rescue; nil; end; p x"
 assert "42", "x = begin; raise; nil; rescue; 42; end; p x"
 assert "42", "x = begin; 42; rescue; nil; ensure; nil; end; p x"
