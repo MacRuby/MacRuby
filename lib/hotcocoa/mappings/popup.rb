dcc2688a29c43e397dd5df0f088ea80994a8b35d
Richard Kilmer <rich@infoether.com>
2009-08-25 05:17:45 +0000

    move classes outside of the custom_methods
    git-svn-id: http://svn.macosforge.org/repository/ruby/MacRuby/trunk@2368 23306eb0-4c56-4727-a40e-e92c0eb68959

--- lib/hotcocoa/mappings/popup.rb
+++ lib/hotcocoa/mappings/popup.rb
@@ -1,3 +1,85 @@
+module HotCocoa
+  class PopUpItemList
+    
+    include Enumerable
+    
+    attr_reader :control
+    
+    def initialize(control)
+      @control = control
+    end
+    
+    def <<(title)
+      control.addItemWithTitle(title)
+    end
+    
+    def [](index)
+      control.itemTitleAtIndex(index)
+    end
+    
+    def delete(title)
+      if title.kind_of?(Fixnum)
+        control.removeItemAtIndex(title)
+      else
+        control.removeItemWithTitle(title)
+      end
+    end
+    
+    def insert(title, at:index)
+      control.insertItemWithTitle(title, atIndex:index)
+    end
+    
+    def selected
+      control.titleOfSelectedItem
+    end
+    
+    def selected=(title)
+      if title.kind_of?(Fixnum)
+        control.selectItemAtIndex(title)
+      else
+        control.selectItemWithTitle(title)
+      end
+    end
+    
+    def selected_index
+      control.indexOfSelectedItem
+    end
+    
+    def size
+      control.numberOfItems
+    end
+    
+    def each(&block)
+      control.itemTitles.each(&block)
+    end
+    
+  end
+  
+  class PopUpMenuItemList < PopUpItemList
+    include Enumerable
+    
+    def selected
+      control.selectedItem
+    end
+
+    def selected=(menu_item)
+      if menu_item.kind_of?(Fixnum)
+        control.selectItemAtIndex(menu_item)
+      else
+        control.selectItem(menu_item)
+      end
+    end
+
+    def [](index)
+      control.itemAtIndex(index)
+    end
+    
+    def each(&block)
+      control.itemArray.each(&block)
+    end
+  end
+end
+
 HotCocoa::Mappings.map :popup => :NSPopUpButton do
   
   defaults :pulls_down => false, 
@@ -10,97 +92,17 @@ HotCocoa::Mappings.map :popup => :NSPopUpButton do
   
   custom_methods do
     
-    class ItemList
-      
-      include Enumerable
-      
-      attr_reader :control
-      
-      def initialize(control)
-        @control = control
-      end
-      
-      def <<(title)
-        control.addItemWithTitle(title)
-      end
-      
-      def [](index)
-        control.itemTitleAtIndex(index)
-      end
-      
-      def delete(title)
-        if title.kind_of?(Fixnum)
-          control.removeItemAtIndex(title)
-        else
-          control.removeItemWithTitle(title)
-        end
-      end
-      
-      def insert(title, at:index)
-        control.insertItemWithTitle(title, atIndex:index)
-      end
-      
-      def selected
-        control.titleOfSelectedItem
-      end
-      
-      def selected=(title)
-        if title.kind_of?(Fixnum)
-          control.selectItemAtIndex(title)
-        else
-          control.selectItemWithTitle(title)
-        end
-      end
-      
-      def selected_index
-        control.indexOfSelectedItem
-      end
-      
-      def size
-        control.numberOfItems
-      end
-      
-      def each(&block)
-        control.itemTitles.each(&block)
-      end
-      
-    end
-    
-    class MenuItemList < ItemList
-      include Enumerable
-      
-      def selected
-        control.selectedItem
-      end
-
-      def selected=(menu_item)
-        if menu_item.kind_of?(Fixnum)
-          control.selectItemAtIndex(menu_item)
-        else
-          control.selectItem(menu_item)
-        end
-      end
-
-      def [](index)
-        control.itemAtIndex(index)
-      end
-      
-      def each(&block)
-        control.itemArray.each(&block)
-      end
-    end
-    
     def items=(values)
       removeAllItems
       addItemsWithTitles(values)
     end
     
     def items
-      @_item_list ||= ItemList.new(self)
+      @_item_list ||= HotCocoa::PopUpItemList.new(self)
     end
     
     def menu_items
-      @_menu_item_list ||=  MenuItemList.new(self)
+      @_menu_item_list ||=  HotCocoa::PopUpMenuItemList.new(self)
     end
     
   end
