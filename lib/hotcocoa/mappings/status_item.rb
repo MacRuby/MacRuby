dc79169a7db620813af7d04d435214ff3f0e33a8
Richard Kilmer <rich@infoether.com>
2009-08-25 04:00:07 +0000

    fix ticket #169
    git-svn-id: http://svn.macosforge.org/repository/ruby/MacRuby/trunk@2365 23306eb0-4c56-4727-a40e-e92c0eb68959

--- lib/hotcocoa/mappings/status_item.rb
+++ lib/hotcocoa/mappings/status_item.rb
@@ -3,7 +3,7 @@ HotCocoa::Mappings.map :status_item => :NSStatusItem do
   defaults :length => NSVariableStatusItemLength
 
   def alloc_with_options(options)
-    status_bar.statusItemWithLength options.delete(:length)
+    HotCocoa.status_bar.statusItemWithLength options.delete(:length) 
   end
 
 end
