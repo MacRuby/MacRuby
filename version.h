b205a6d03420fd7bff178511644c414dc645504c
Laurent Sansonetti <lsansonetti@apple.com>
2009-08-29 02:51:08 +0000

    bumb RUBY_VERSION to 1.9.2
    git-svn-id: http://svn.macosforge.org/repository/ruby/MacRuby/trunk@2422 23306eb0-4c56-4727-a40e-e92c0eb68959

--- version.h
+++ version.h
@@ -1,7 +1,7 @@
 #define RUBY_ENGINE 		"macruby"
 #define MACRUBY_VERSION 	0.5
 
-#define RUBY_VERSION 		"1.9.0"
+#define RUBY_VERSION 		"1.9.2"
 #define RUBY_RELEASE_DATE 	"2008-06-03"
 #define RUBY_VERSION_CODE 	190
 #define RUBY_RELEASE_CODE 	20080603
