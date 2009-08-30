58c1b0975e08f5ab51766cb5b2ac14e78a7cb942
Eloy Duran <eloy.de.enige@gmail.com>
2009-08-30 01:14:37 +0000

    Added failing test_vm test about method_defined? for an aliased method
    git-svn-id: http://svn.macosforge.org/repository/ruby/MacRuby/trunk@2434 23306eb0-4c56-4727-a40e-e92c0eb68959

--- test_vm/alias.rb
+++ test_vm/alias.rb
@@ -2,3 +2,11 @@ assert "42", "$foo = 42; alias $bar $foo; p $bar"
 assert "nil", "alias $bar $foo; p $bar"
 
 assert "42", "def foo; 42; end; alias :bar :foo; p bar"
+
+assert "true", %{
+  class X
+    def foo; end
+    alias bar foo
+  end
+  p X.method_defined?(:bar)
+}
