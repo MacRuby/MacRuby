a835183eb160b0f0ce297ca119a1113bba21e5fc
Laurent Sansonetti <lsansonetti@apple.com>
2009-09-17 21:42:21 +0000

    expose Symbol.all_symbols
    git-svn-id: http://svn.macosforge.org/repository/ruby/MacRuby/trunk@2555 23306eb0-4c56-4727-a40e-e92c0eb68959

--- string.c
+++ string.c
@@ -5795,7 +5795,7 @@ Init_String(void)
 
     rb_undef_alloc_func(rb_cSymbol);
     rb_undef_method(CLASS_OF(rb_cSymbol), "new");
-    rb_define_singleton_method(rb_cSymbol, "all_symbols", rb_sym_all_symbols, 0); /* in parse.y */
+    rb_objc_define_method(*(VALUE *)rb_cSymbol, "all_symbols", rb_sym_all_symbols, 0); /* in parse.y */
 
     rb_objc_define_method(rb_cSymbol, "==", sym_equal, 1);
     rb_objc_define_method(rb_cSymbol, "eql?", sym_equal, 1);
