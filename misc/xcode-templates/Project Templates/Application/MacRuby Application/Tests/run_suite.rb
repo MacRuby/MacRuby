0a9b03dbb49eee090bc0b1714a5c4c664ce9708d
Eloy Duran <eloy.de.enige@gmail.com>
2009-10-05 17:50:00 +0000

    Test additions to the application template are thanks to Dylan Bruzenak. #362.
    Removed some whitespace and fixed my error of failing at including the author
    again...
    
    git-svn-id: http://svn.macosforge.org/repository/ruby/MacRuby/trunk@2724 23306eb0-4c56-4727-a40e-e92c0eb68959

--- misc/xcode-templates/Project Templates/Application/MacRuby Application/Tests/run_suite.rb
+++ misc/xcode-templates/Project Templates/Application/MacRuby Application/Tests/run_suite.rb
@@ -1,2 +1,1 @@
-
 Dir.glob(File.expand_path('../**/*_test.rb', __FILE__)).each { |test| require test }
--- misc/xcode-templates/Project Templates/Application/MacRuby Application/Tests/stub_test.rb
+++ misc/xcode-templates/Project Templates/Application/MacRuby Application/Tests/stub_test.rb
@@ -3,15 +3,15 @@ require 'test/unit'
 #require 'ruby_file_to_test'
 
 class SimpleTest < Test::Unit::TestCase
-	def setup
-		puts 'setup called'
-	end	
-	
-	def teardown
-		puts 'teardown called'
-	end
-	
-	def test_fail
-		assert(false, 'Assertion was false.')
-	end
+  def setup
+    puts 'setup called'
+  end
+  
+  def teardown
+    puts 'teardown called'
+  end
+  
+  def test_fail
+    assert false, 'Assertion was false.'
+  end
 end
