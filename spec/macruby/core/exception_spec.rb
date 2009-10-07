275b7eefbf4e21ba16ef9d34672b0fba5dfab820
Laurent Sansonetti <lsansonetti@apple.com>
2009-10-07 20:46:23 +0000

    uncomment specs that now pass
    git-svn-id: http://svn.macosforge.org/repository/ruby/MacRuby/trunk@2748 23306eb0-4c56-4727-a40e-e92c0eb68959

--- spec/macruby/core/exception_spec.rb
+++ spec/macruby/core/exception_spec.rb
@@ -2,16 +2,12 @@ require File.dirname(__FILE__) + "/../spec_helper"
 FixtureCompiler.require! "exception"
 TestException # force dynamic load
 
-=begin # TODO
 describe "An Objective-C exception" do
   it "can be catched from Ruby" do
     lambda { TestException.raiseObjCException }.should raise_error
   end
 end
-=end
 
-# TODO: this is not implemented for i386
-if RUBY_ARCH == 'x86_64'
 describe "A Ruby exception" do
   it "can be catched from Objective-C" do
     o = Object.new
@@ -20,5 +16,4 @@ describe "A Ruby exception" do
     end
     TestException.catchRubyException(o).should == 1
   end
-end
 end
