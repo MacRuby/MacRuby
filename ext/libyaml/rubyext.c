206f833ccf475102366bc1888f1d11fd7a8bd3e3
Matthias Neeracher <neeracher@apple.com>
2009-10-16 06:10:26 +0000

    Eliminate signedness warning
    git-svn-id: http://svn.macosforge.org/repository/ruby/MacRuby/trunk@2830 23306eb0-4c56-4727-a40e-e92c0eb68959

--- ext/libyaml/rubyext.c
+++ ext/libyaml/rubyext.c
@@ -729,7 +729,7 @@ rb_yaml_emitter_scalar(VALUE self, SEL sel, VALUE taguri, VALUE val,
 
     int can_omit_tag = 0;
     yaml_char_t *tag = rb_yaml_tag_or_null(taguri, &can_omit_tag);
-    yaml_scalar_event_initialize(&ev, NULL, tag, output, strlen(output),
+    yaml_scalar_event_initialize(&ev, NULL, tag, output, strlen((const char *)output),
 	    can_omit_tag, can_omit_tag, rb_symbol_to_scalar_style(style));
     yaml_emitter_emit(emitter, &ev);
 
