f988b29c08178359bd418ae5327f4d7d62305b9b
Eloy Duran <eloy.de.enige@gmail.com>
2009-11-04 12:13:02 +0000

    Added simple xcode/ib sample app, to show how you get going. App by Robert Beekman.
    git-svn-id: http://svn.macosforge.org/repository/ruby/MacRuby/trunk@2951 23306eb0-4c56-4727-a40e-e92c0eb68959

---
+++ sample-macruby/WhoIsGettingLunch/main.m
@@ -0,0 +1,14 @@
+//
+//  main.m
+//  whoisgettinglunch
+//
+//  Created by Robert Beekman on 04-11-09.
+//  Copyright __MyCompanyName__ 2009. All rights reserved.
+//
+
+#import <MacRuby/MacRuby.h>
+
+int main(int argc, char *argv[])
+{
+    return macruby_main("rb_main.rb", argc, argv);
+}
