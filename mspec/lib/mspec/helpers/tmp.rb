afe40f35ac78bd8e8c5e693174eb3b475a16b301
Eloy Duran <eloy.de.enige@gmail.com>
2009-11-16 23:45:16 +0000

    Updated MSpec to bcec47c70e0678a29fd0c1345358c4daf7b971a3
    git-svn-id: http://svn.macosforge.org/repository/ruby/MacRuby/trunk@3024 23306eb0-4c56-4727-a40e-e92c0eb68959

--- mspec/lib/mspec/helpers/tmp.rb
+++ mspec/lib/mspec/helpers/tmp.rb
@@ -20,7 +20,7 @@ class Object
       if RUBY_NAME =~ /^macruby/
         @spec_temp_directory = "/private/tmp"
       else
-        [ "/private/tmp", "/tmp", "/var/tmp", ENV["TMPDIR"], ENV["TMP"],
+        [ ENV["TMPDIR"], "/private/tmp", "/tmp", "/var/tmp", ENV["TMP"],
           ENV["TEMP"], ENV["USERPROFILE"] ].each do |dir|
           if dir and File.directory?(dir) and File.writable?(dir)
             temp = File.expand_path dir
