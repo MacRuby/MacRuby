1edcf421a9911d4b7e849b721a00e4501b09136b
Laurent Sansonetti <lsansonetti@apple.com>
2009-11-30 06:35:37 +0000

    added MacOnRack example (from http://github.com/josh/MacOnRack)
    git-svn-id: http://svn.macosforge.org/repository/ruby/MacRuby/trunk@3062 23306eb0-4c56-4727-a40e-e92c0eb68959

---
+++ sample-macruby/MacOnRack/main.m
@@ -0,0 +1,14 @@
+//
+//  main.m
+//  MacOnRack
+//
+//  Created by Joshua Peek on 11/20/09.
+//  Copyright 37signals 2009. All rights reserved.
+//
+
+#import <MacRuby/MacRuby.h>
+
+int main(int argc, char *argv[])
+{
+    return macruby_main("rb_main.rb", argc, argv);
+}
