590766b017d25899fa03c0c5570a8e06e61c2500
Matthias Neeracher <neeracher@apple.com>
2010-01-19 04:36:38 +0000

    Made dump_stream_tags test pass by fixing overdetermined test
    git-svn-id: http://svn.macosforge.org/repository/ruby/MacRuby/trunk@3297 23306eb0-4c56-4727-a40e-e92c0eb68959

--- spec/frozen/library/yaml/dump_stream_spec.rb
+++ spec/frozen/library/yaml/dump_stream_spec.rb
@@ -6,7 +6,11 @@ describe "YAML.dump_stream" do
     YAML.dump_stream.should == ""
   end
 
+  # 
+  # We used to compare against []\n\n and {}\n\n, but this was overdetermining the format 
+  # just to match Syck
+  #
   it "returns a YAML stream containing the objects passed" do
-    YAML.dump_stream('foo', 20, [], {}).should == "--- foo\n--- 20\n--- []\n\n--- {}\n\n"
+    YAML.dump_stream('foo', 20, [], {}).should =~ /--- foo\n--- 20\n--- \[\]\n+--- \{\}\n+/
   end
 end
