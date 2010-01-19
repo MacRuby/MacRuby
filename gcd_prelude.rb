a788a4dee9053d17832c59289a02f782dd05f769
Dr. Ernie Prabhakar <ernest.prabhakar@gmail.com>
2010-01-19 23:56:53 +0000

    Add gcd_prelude rdoc
    git-svn-id: http://svn.macosforge.org/repository/ruby/MacRuby/trunk@3305 23306eb0-4c56-4727-a40e-e92c0eb68959

--- gcd_prelude.rb
+++ gcd_prelude.rb
@@ -32,32 +32,41 @@ module Dispatch
   end
   
   class Queue
+    # Returns a Dispatch::Source::DATA_ADD
     def on_add(&block)
       Dispatch::Source.new(Dispatch::Source::DATA_ADD, 0, 0, self, &block)
     end
 
+    # Returns a Dispatch::Source::DATA_OR
     def on_or(&block)
       Dispatch::Source.new(Dispatch::Source::DATA_OR, 0, 0, self, &block)
     end
 
+    # Takes event symbols: :delete, :write, :extend, :attrib, :link, :rename, :revoke
+    # Returns a Dispatch::Source::PROC
     def on_process_event(pid, *events, &block)
       mask = events.collect {|e| Dispatch::Source::proc_event(e)}.reduce(:|)
       Dispatch::Source.new(Dispatch::Source::PROC, pid, mask, self, &block)
     end
 
+    # Returns a Dispatch::Source::SIGNAL
     def on_signal(signal, &block)
       signal = Signal.list[signal.to_s] if signal.to_i == 0
       Dispatch::Source.new(Dispatch::Source::SIGNAL, signal, 0, self, &block)
     end
 
+    # Returns a Dispatch::Source::READ
     def on_read(file, &block)
       Dispatch::Source.new(Dispatch::Source::READ, file, 0, self, &block)
     end
 
+    # Returns a Dispatch::Source::WRITE
     def on_write(file, &block)
       Dispatch::Source.new(Dispatch::Source::WRITE, file, 0, self, &block)
     end
 
+    # Takes event symbols: :exit, :fork, :exec, :signal
+    # Returns a Dispatch::Source::VNODE
     def on_file_event(file, *events, &block)
       mask = events.collect {|e| Dispatch::Source::vnode_event(e)}.reduce(:|)
       Dispatch::Source.new(Dispatch::Source::VNODE, file,mask, self,&block)
