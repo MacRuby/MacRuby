e0b3df4129eb89fc44998bd60ffb2d2ca7042771
Dr. Ernie Prabhakar <ernest.prabhakar@gmail.com>
2010-02-08 23:03:39 +0000

    Moved Dispatch::Enumerable directly into Enumerable
    git-svn-id: http://svn.macosforge.org/repository/ruby/MacRuby/trunk@3444 23306eb0-4c56-4727-a40e-e92c0eb68959

--- lib/dispatch/enumerable.rb
+++ lib/dispatch/enumerable.rb
@@ -1,69 +1,61 @@
 # Additional parallel operations for any object supporting +each+
 
-module Dispatch
-  module Enumerable
-    # Parallel +each+
-    def p_each(&block)
-      grp = Group.new
-      self.each do |obj|
-        Dispatch.group(grp) { block.call(obj) }        
-      end
-      grp.wait
+module Enumerable
+  # Parallel +each+
+  def p_each(&block)
+    grp = Dispatch::Group.new
+    self.each do |obj|
+      Dispatch.group(grp) { block.call(obj) }        
     end
+    grp.wait
+  end
 
-    # Parallel +each_with_index+
-    def p_each_with_index(&block)
-      grp = Group.new
-      self.each_with_index do |obj, i|
-        Dispatch.group(grp) { block.call(obj, i) }
-      end
-      grp.wait
+  # Parallel +each_with_index+
+  def p_each_with_index(&block)
+    grp = Dispatch::Group.new
+    self.each_with_index do |obj, i|
+      Dispatch.group(grp) { block.call(obj, i) }
     end
+    grp.wait
+  end
 
-    # Parallel +inject+
-    # Requires initial value since 'first' no longer special.
-    # Only works if result doesn't depend on the order elements are processed.
-    def p_inject(initial, &block)
-      @result = Dispatch.wrap(initial)
-      self.p_each { |obj| block.call(@result, obj) }
-      @result._done
-      return @result
-    end
+  # Parallel +inject+
+  # Requires initial value since 'first' no longer special.
+  # Only works if result doesn't depend on the order elements are processed.
+  def p_inject(initial, &block)
+    @result = Dispatch.wrap(initial)
+    self.p_each { |obj| block.call(@result, obj) }
+    @result._done
+    return @result
+  end
 
-    # Parallel +collect+
-    def p_map(&block)
-      result = Dispatch.wrap(Array)
-      self.p_each_with_index do |obj, i|
-        result[i] = block.call(obj)
-      end
-      result
+  # Parallel +collect+
+  def p_map(&block)
+    result = Dispatch.wrap(Array)
+    self.p_each_with_index do |obj, i|
+      result[i] = block.call(obj)
     end
+    result
+  end
 
-    # Parallel +select+; will return array of objects for which
-    # +&block+ returns true.
-    # Useful if the test block is very expensive to run
-    def p_find_all(&block)
-      @found_all = Dispatch.wrap(Array)
-      self.p_each { |obj| @found_all << obj if block.call(obj) }
-      @found_all._done # will this leak?
-    end
+  # Parallel +select+; will return array of objects for which
+  # +&block+ returns true.
+  # Useful if the test block is very expensive to run
+  def p_find_all(&block)
+    @found_all = Dispatch.wrap(Array)
+    self.p_each { |obj| @found_all << obj if block.call(obj) }
+    @found_all._done # will this leak?
+  end
 
-    # Parallel +detect+; will return -one- match for +&block+
-    # but it may not be the 'first'
-    # Useful if the test block is very expensive to run
-    def p_find(&block)
-      @found = Dispatch.wrap(nil)
-      self.p_each do |obj|
-        found = @found.nil? ? block.call(obj) : nil
-        @found = obj if found and @found.nil? 
-      end
-      @found._done # will this leak?
+  # Parallel +detect+; will return -one- match for +&block+
+  # but it may not be the 'first'
+  # Useful if the test block is very expensive to run
+  def p_find(&block)
+    @found = Dispatch.wrap(nil)
+    self.p_each do |obj|
+      found = @found.nil? ? block.call(obj) : nil
+      @found = obj if found and @found.nil? 
     end
+    @found._done # will this leak?
   end
 end
-
-# Mixin to Enumerable, and thus everything that includes it
-module Enumerable
-  include Dispatch::Enumerable
-end
-
