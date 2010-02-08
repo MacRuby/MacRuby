def54eca43294d6ace8c2a27ee89ec406546202b
Dr. Ernie Prabhakar <ernest.prabhakar@gmail.com>
2010-02-08 23:03:48 +0000

    Initial enumerable spec
    git-svn-id: http://svn.macosforge.org/repository/ruby/MacRuby/trunk@3445 23306eb0-4c56-4727-a40e-e92c0eb68959

---
+++ spec/macruby/library/dispatch/enumerable_spec.rb
@@ -0,0 +1,68 @@
+require File.dirname(__FILE__) + "/../../spec_helper"
+require 'dispatch'
+
+if MACOSX_VERSION >= 10.6
+  describe "parallel Enumerable" do
+    before :each do
+      @ary = (1..3).to_a
+    end
+
+    describe :p_each do
+      it "exists on objects that support Enumerable" do
+        @ary.respond_to?(:p_each).should == true
+      end
+
+      it "behaves like each" do
+        @sum1 = 0
+        @ary.each {|v| @sum1 += v*v}
+        @sum2 = 0
+        @q = Dispatch.queue_for(@sum2)
+        @ary.p_each {|v| temp = v*v; @q.sync {@sum2 += temp} }
+        @sum1.should == @sum2
+      end
+      
+      it "executes concurrently" do
+        true.should == true
+      end
+    end
+    
+    describe :p_each_with_index do
+      it "exists on objects that support Enumerable" do
+        @ary.respond_to?(:p_each).should == true
+      end
+      it "behaves like each_with_index" do
+        @ary.should.respond_to? :p_each_with_index
+      end
+      
+      it "executes concurrently" do
+        true.should == true
+      end
+    end
+    
+    describe :p_inject do
+      it "exists on objects that support Enumerable" do
+        @ary.respond_to?(:p_each).should == true
+      end
+      it "behaves like inject" do
+        @ary.should.respond_to? :p_inject
+      end
+      
+      it "executes concurrently" do
+        true.should == true
+      end
+    end
+
+    describe :p_map do
+      it "exists on objects that support Enumerable" do
+        @ary.respond_to?(:p_each).should == true
+      end
+      it "behaves like map" do
+        @ary.should.respond_to? :p_map
+      end
+      
+      it "executes concurrently" do
+        true.should == true
+      end
+    end
+  end
+end
