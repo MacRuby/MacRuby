110744d8f56324447e9b68d813019aecb53b7089
Dr. Ernie Prabhakar <ernest.prabhakar@gmail.com>
2010-02-12 19:24:10 +0000

    Annoted dispatch sample with output
    git-svn-id: http://svn.macosforge.org/repository/ruby/MacRuby/trunk@3517 23306eb0-4c56-4727-a40e-e92c0eb68959

--- sample-macruby/Scripts/gcd/dispatch_methods.rb
+++ sample-macruby/Scripts/gcd/dispatch_methods.rb
@@ -3,43 +3,44 @@ require 'dispatch'
 
 puts "\n Use Dispatch.async to do stuff in the background"
 Dispatch.async { p "Did this later" }
-sleep 0.1
+sleep 0.1 # => "Did this later"
 
 puts "\n Use Dispatch.group to track when stuff completes"
 g = Dispatch.group { p "Do this" }
 Dispatch.group(g) { p "and that" }
-g.wait
+g.wait # => "Do this" "and that"
 p "Done"
 
 puts "\n Use Dispatch.fork to capture return values in a Future"
 f = Dispatch.fork {  2+2  }
-p f.value
+p f.value # => 4
 puts "  - pass a block to return the value asynchronously"
 f.value { |v| p "Returns #{v}" }
-sleep 0.1
+sleep 0.1  # => "Returns 4"
 
 puts "\n Use Dispatch.queue_for to create a private serial queue"
 puts "  - synchronizes access to shared data structures"
 a = Array.new
 q = Dispatch.queue_for(a)
 puts "  - has a (mostly) unique name:"
-p q
+p q # => Dispatch.enumerable.array.0x2000a6920.1266002369.9854
 q.async { a << "change me"  }
 puts "  - uses sync to block and flush queue"
-q.sync { p a }
+q.sync { p a } # => ["change me"]
 
 puts "\n Use with a group for more complex dependencies, "
-q.async(g) { a << "more change"  }
+q.async(g) { a << "more change" }
 Dispatch.group(g) do 
   tmp = "complex calculation"
   q.async(g) { a << tmp }
 end
 puts "  - uses notify to execute block when done"
 g.notify(q) { p a }
-q.sync {}
+q.sync {} # => ["change me", "more change", "complex calculation"]
 
 puts "\n Use Dispatch.wrap to serialize object using an Actor"
 b = Dispatch.wrap(Array)
 b << "safely change me"
 p b.size # => 1 (synchronous return)
-b.size {|n| p "Size=#{n}"} # => "Size=1" (asynchronous return)
+b.size {|n| p "Size=#{n}"}
+sleep 0.1  # => "Size=1" (asynchronous return)
