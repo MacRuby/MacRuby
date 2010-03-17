8eb1a6e1c6c7689263e8fddd77ce6945a8bdcb51
Laurent Sansonetti <lsansonetti@apple.com>
2010-03-17 00:32:04 +0000

    fix digest length specs for 1.9, since the constant strings are UTF-8 encoded, their length is not the same
    git-svn-id: http://svn.macosforge.org/repository/ruby/MacRuby/trunk@3781 23306eb0-4c56-4727-a40e-e92c0eb68959

--- spec/frozen/library/digest/sha256/shared/length.rb
+++ spec/frozen/library/digest/sha256/shared/length.rb
@@ -1,8 +1,18 @@
 describe :sha256_length, :shared => true do
-  it 'returns the length of the digest' do
-    cur_digest = Digest::SHA256.new
-    cur_digest.send(@method).should == SHA256Constants::BlankDigest.size
-    cur_digest << SHA256Constants::Contents
-    cur_digest.send(@method).should == SHA256Constants::Digest.size
+  ruby_version_is "" ... "1.9" do
+    it 'returns the length of the digest' do
+      cur_digest = Digest::SHA256.new
+      cur_digest.send(@method).should == SHA256Constants::BlankDigest.size
+      cur_digest << SHA256Constants::Contents
+      cur_digest.send(@method).should == SHA256Constants::Digest.size
+    end
+  end
+  ruby_version_is "1.9" do
+    it 'returns the length of the digest' do
+      cur_digest = Digest::SHA256.new
+      cur_digest.send(@method).should == SHA256Constants::BlankDigest.dup.force_encoding('ascii-8bit').size
+      cur_digest << SHA256Constants::Contents
+      cur_digest.send(@method).should == SHA256Constants::Digest.dup.force_encoding('ascii-8bit').size
+    end
   end
 end
--- spec/frozen/library/digest/sha512/shared/length.rb
+++ spec/frozen/library/digest/sha512/shared/length.rb
@@ -1,8 +1,18 @@
 describe :sha512_length, :shared => true do
-  it 'returns the length of the digest' do
-    cur_digest = Digest::SHA512.new
-    cur_digest.send(@method).should == SHA512Constants::BlankDigest.size
-    cur_digest << SHA512Constants::Contents
-    cur_digest.send(@method).should == SHA512Constants::Digest.size
+  ruby_version_is "" ... "1.9" do
+    it 'returns the length of the digest' do
+      cur_digest = Digest::SHA512.new
+      cur_digest.send(@method).should == SHA512Constants::BlankDigest.size
+      cur_digest << SHA512Constants::Contents
+      cur_digest.send(@method).should == SHA512Constants::Digest.size
+    end
+  end
+  ruby_version_is "1.9" do
+    it 'returns the length of the digest' do
+      cur_digest = Digest::SHA512.new
+      cur_digest.send(@method).should == SHA512Constants::BlankDigest.dup.force_encoding('ascii-8bit').size
+      cur_digest << SHA512Constants::Contents
+      cur_digest.send(@method).should == SHA512Constants::Digest.dup.force_encoding('ascii-8bit').size
+    end
   end
 end
