45d769ab22d719df0833d40e8950ce8d5185a1c3
Dr. Ernie Prabhakar <ernest.prabhakar@gmail.com>
2010-03-24 20:38:06 +0000

    Refactored Benchmark class
    git-svn-id: http://svn.macosforge.org/repository/ruby/MacRuby/trunk@3854 23306eb0-4c56-4727-a40e-e92c0eb68959

--- sample-macruby/Scripts/gcd/benchmarks.rb
+++ sample-macruby/Scripts/gcd/benchmarks.rb
@@ -1,15 +1,24 @@
 #!/usr/local/bin/macruby
 
-require 'benchmark'
 require 'dispatch'
+require 'benchmark'
+
+class Benchmark
+  def self.repeat(count, label="", &block)
+    raise "count: #{count} < 1" if count < 1
+    block.call
+    t = measure {count.times &block} / count
+    Tms.new(*t.to_a[1..-1], label).inspect
+  end
+end
 
 $max_tasks = 256
-$benched = $reps = 100
+$reps = 1024
 $folds = 32
 $results = nil#[]
 
 puts "GCD Benchmarks"
-puts "Tasks: #{$max_tasks}\tFolded: #{$folds}\tReps:\t#{$reps}\n"
+puts "Tasks: #{$max_tasks}\tFolded: #{$folds}\tReps:\t#{$reps}"
 
 def work_function(i)
     x = 1.0+i*i
@@ -29,19 +38,19 @@ def apply(n)
   Dispatch::Queue.concurrent.apply(n) {|i| work_function(i)}
 end
 
-def job(n)
+def concur(n)
   j = Dispatch::Job.new
   n.times {|i| j.add { work_function(i) }}
   j.join
 end
 
-def queue(n)
+def serial(n)
   q = Dispatch::Queue.new('org.macruby.gcd.serial')
   n.times {|i| q.async {work_function(i)}}
   q.sync { } 
 end
 
-def multi(n)
+def multiq(n)
   g = Dispatch::Group.new
   n.times do |i|
     Dispatch::Queue.new("org.macruby.gcd.multi.#{i}").async(g) {work_function(i)}
@@ -49,27 +58,19 @@ def multi(n)
   g.wait
 end
 
-
-def run(x, label, &block)
-  (x.nil?) ? yield : x.report(label) {$reps.times &block}
-end
-
-def run_all(x, n)
-    puts "#{n} tasks" if not x.nil?
-    run(x,"looped") { iter(n) }
-    run(x,"p_loop") { p_iter(n) }
-    run(x," apply") { apply(n) }
-    run(x,"concur") { job(n) }
-    run(x,"serial") { queue(n) }
-    run(x,"multiq") { multi(n) }
-    puts
+def bench(method, count=1)
+  proc = Proc.new { send(method.to_sym, count) }
+  t = Benchmark.measure("%6s" % method, &proc)
+  t_msec = t.real*1000
+  puts "#{t.label}: %5.2f millisec (avg: %5.2f microsec)" % [t_msec, t_msec*1000/count]
 end
 
 n = 1
-width = 6
 while n <= $max_tasks do
-  run_all(nil, n)
-  Benchmark.bm(width) { |x| run_all(x, n) }
+  puts "\n#{n} tasks"
+  %w(iter p_iter apply concur serial multiq).each do |s|
+    bench(s, n)
+  end
   n *= 2
 end
 
