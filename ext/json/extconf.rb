b286e5b22cea2d6e314b00f91ccd4a5265b6504e
Matthias Neeracher <neeracher@apple.com>
2010-04-14 14:15:39 +0000

    -std=c99 -> -std=gnu99 (Because some system include files may include language extensions)
    git-svn-id: http://svn.macosforge.org/repository/ruby/MacRuby/trunk@3931 23306eb0-4c56-4727-a40e-e92c0eb68959

--- ext/json/extconf.rb
+++ ext/json/extconf.rb
@@ -3,6 +3,6 @@ require 'mkmf'
 require 'rbconfig'
 
 $INCFLAGS << ' -I../..'
-$CFLAGS << ' -Wall -std=c99'
+$CFLAGS << ' -Wall -std=gnu99'
 
 create_makefile("json")
--- ext/ripper/extconf.rb
+++ ext/ripper/extconf.rb
@@ -17,7 +17,7 @@ def main
   $VPATH << '$(topdir)' << '$(top_srcdir)'
   $INCFLAGS << ' -I$(topdir) -I$(top_srcdir) -I$(top_srcdir)/onig'
   $INCFLAGS << ' -I../../icu-1060' if `sw_vers -productVersion`.to_f <= 10.6
-  $CFLAGS << ' -std=c99'
+  $CFLAGS << ' -std=gnu99'
   create_makefile 'ripper'
 end
 
