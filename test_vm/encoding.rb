8462a30e7ab97d5340742820cb95bc9083af25b1
Eloy Duran <eloy.de.enige@gmail.com>
2010-05-26 20:30:35 +0000

    Add failing test about Array#pack returning a string with the wrong encoding.
    git-svn-id: http://svn.macosforge.org/repository/ruby/MacRuby/trunk@4164 23306eb0-4c56-4727-a40e-e92c0eb68959

--- test_vm/encoding.rb
+++ test_vm/encoding.rb
@@ -1,3 +1,5 @@
 assert "US-ASCII", "File.open('../Rakefile', 'r:US-ASCII') {|f| puts f.read.encoding.name }"
 
 assert ":ok", "puts ':ok'.encode('US-ASCII')"
+
+assert "true", "p [].pack('U*').encoding.name == 'UTF-8'"
