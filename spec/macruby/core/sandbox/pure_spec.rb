02099e39b102852424c8b667013a0a4e2772c59a
Patrick Thomson <pthomson@apple.com>
2010-07-16 17:27:59 +0000

    More tests thanks to mspec's ruby_exe function.
    git-svn-id: http://svn.macosforge.org/repository/ruby/MacRuby/trunk@4368 23306eb0-4c56-4727-a40e-e92c0eb68959

--- spec/macruby/core/sandbox/pure_spec.rb
+++ spec/macruby/core/sandbox/pure_spec.rb
@@ -2,8 +2,24 @@ require File.expand_path(File.dirname(__FILE__) + '/../../spec_helper')
 
 describe "Sandbox.pure_computation" do
   
-  # More specs coming to this space soon. Right now applying a sandbox profile
-  # inside a spec causes all subsequent specs to fail.
+  before(:each) do
+    @code = "Sandbox.pure_computation.apply!; "
+  end
+  
+  it "should disallow spawning new processes" do
+    @code << "IO.popen('whoami')"
+    ruby_exe(@code).should =~ /posix_spawn\(\) failed/
+  end
+  
+  it "should disallow open()" do
+    @code << "open('/dev/null')"
+    ruby_exe(@code).should =~ /open\(\) failed/
+  end
+  
+  it "should disallow IO.read" do
+    @code << "p IO.read('/dev/urandom')"
+    ruby_exe(@code).should =~ /open\(\) failed/
+  end
   
   it "should be frozen" do
     Sandbox.pure_computation.frozen?.should be_true
