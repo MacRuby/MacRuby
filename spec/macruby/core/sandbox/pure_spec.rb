f9aaf7fde123b8f37d7ca0e1914e22a35d4cb133
Patrick Thomson <pthomson@apple.com>
2010-07-16 17:54:55 +0000

    More tests for Sandbox.pure_computation
    git-svn-id: http://svn.macosforge.org/repository/ruby/MacRuby/trunk@4369 23306eb0-4c56-4727-a40e-e92c0eb68959

--- spec/macruby/core/sandbox/pure_spec.rb
+++ spec/macruby/core/sandbox/pure_spec.rb
@@ -2,6 +2,14 @@ require File.expand_path(File.dirname(__FILE__) + '/../../spec_helper')
 
 describe "Sandbox.pure_computation" do
   
+  # These tests are applicable beyond just the pure_computation spec.
+  # Eventually the tests themselves will be farmed out to /sandbox/shared
+  # and all sandbox specs will just be aggregations of should_behave_like calls.
+  
+  before(:all) do
+    @filename = File.expand_path(File.dirname(__FILE__) + '/../../spec_helper.rb')
+  end
+  
   before(:each) do
     @code = "Sandbox.pure_computation.apply!; "
   end
@@ -19,6 +27,26 @@ describe "Sandbox.pure_computation" do
   it "should disallow IO.read" do
     @code << "p IO.read('/dev/urandom')"
     ruby_exe(@code).should =~ /open\(\) failed/
+  end
+  
+  it "should disallow most NSFileManager methods" do
+    @code << "p NSFileManager.defaultManager.currentDirectoryPath"
+    ruby_exe(@code).should =~ /nil/
+  end
+  
+  it "should disallow NSString.stringWithContentsOfFile" do
+    @code << "p NSString.stringWithContentsOfFile('#{@filename}')"
+    ruby_exe(@code).should =~ /nil/
+  end
+  
+  it "should disallow NSString.writeToFile" do
+    @code << "p 'hello'.writeToFile('#{@filename}', atomically:true)"
+    ruby_exe(@code).should =~ /0/
+  end
+  
+  it "should disallow most NSWorkspace methods" do
+    @code = "framework 'Cocoa'; " + @code + "p NSWorkspace.sharedWorkspace.launchApplication('Finder')"
+    ruby_exe(@code).should =~ /false/
   end
   
   it "should be frozen" do
