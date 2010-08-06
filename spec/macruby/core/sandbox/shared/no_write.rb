184a6d542d65a9c4735d7a65711b8de8d3ff47ec
Patrick Thomson <pthomson@apple.com>
2010-08-06 20:57:17 +0000

    Big cleanup, much better-specified and cleaner tests
    git-svn-id: http://svn.macosforge.org/repository/ruby/MacRuby/trunk@4412 23306eb0-4c56-4727-a40e-e92c0eb68959

--- spec/macruby/core/sandbox/shared/no_write.rb
+++ spec/macruby/core/sandbox/shared/no_write.rb
@@ -1,22 +1,10 @@
 describe :sandbox_no_write, :shared => true do
   
-  before do
-    @code = "error = Pointer.new_with_type('@'); "
-    @filename = fixture('spec/macruby/core/sandbox/shared', 'sample_file.txt')
+  it "prevents NSString#writeToFile from writing any characters" do
+    with_temporary_file do |tmp|
+      add_line "print 'hello'.writeToFile('#{tmp}', atomically:true)"
+      result.to_i.should == 0
+    end
   end
   
-  it "prevents Objective-C methods from writing to a file" do
-    @code << "print 'hello'.writeToFile('#{@filename}', atomically:true)"
-    ruby_exe(@code).to_i.should == 0
-  end
-  
-  it "prevents Ruby methods from writing to a file" do
-    @code << "open('#{@filename}'); file.puts 'this must fail'"
-    ruby_exe(@code).should =~ /Errno::EPERM/
-  end
-  
-  it "prevents otherwise changing file attributes through the File module" do
-    @code << "File.chmod(0777, '#{@filename}')"
-    ruby_exe(@code).should =~ /Errno::EPERM/
-  end
 end
