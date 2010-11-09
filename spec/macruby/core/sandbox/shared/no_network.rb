4d5ad007f7f0b54f53be264d8c58b6a011929f67
Watson <watson1978@gmail.com>
2010-11-09 09:38:23 +0000

    modified the spec of Sandbox.
    git-svn-id: http://svn.macosforge.org/repository/ruby/MacRuby/trunk@4897 23306eb0-4c56-4727-a40e-e92c0eb68959

--- spec/macruby/core/sandbox/shared/no_network.rb
+++ spec/macruby/core/sandbox/shared/no_network.rb
@@ -1,7 +1,7 @@
 describe :sandbox_no_network, :shared => true do
   
   it "disallows DNS requests through NSHost" do
-    add_line 'print NSHost.hostWithName("apple.com").address'
+    add_line 'begin; print NSHost.hostWithName("apple.com").address; rescue; print ""; end'
     result.should be_empty
   end
   
