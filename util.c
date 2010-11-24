9614ee6f8ad7e841b9781ec3c0ee89dd9523bff1
Watson <watson1978@gmail.com>
2010-11-24 15:25:23 +0000

    fixed the bug of ruby_strtod().
    Test Script:
    {{{
    require 'test/unit/assertions.rb'
    include Test::Unit::Assertions
    
    assert_raise(ArgumentError){ "%E" % "10." }
    
    puts :ok
    }}}
    
    git-svn-id: http://svn.macosforge.org/repository/ruby/MacRuby/trunk@4941 23306eb0-4c56-4727-a40e-e92c0eb68959

--- util.c
+++ util.c
@@ -2280,6 +2280,9 @@ break2:
     }
 #endif
     if (c == '.') {
+        if (!ISDIGIT(s[1])) {
+            goto dig_done;
+	}
         c = *++s;
         if (!nd) {
             for (; c == '0'; c = *++s)
