MacRuby/MacRuby

commit b55013902bb8586f1cf972aa7ce2d0948d8431a4

32-bit: fix an ABI issue when calling a C function returning a struct that can fit inside a 64-bit integer (such as NSPoint)
git-svn-id: http://svn.macosforge.org/repository/ruby/MacRuby/trunk@4952 23306eb0-4c56-4727-a40e-e92c0eb68959

compiler.cpp
@@ -6024,6 +6024,18 @@ RoxorCompiler::compile_stub(const char *types, bool variadic, int min_argc,
 	ret_type = VoidTy;
     }
 
+#if !__LP64__
+    const Type *small_struct_type = NULL;
+    if (ret_type->getTypeID() == Type::StructTyID
+	    && GET_CORE()->get_sizeof(ret_type) == 8) {
+	// We are returning a small struct that can fit inside a 64-bit
+	// integer (such as NSPoint).
+	// TODO: we should probably make this more generic.
+	small_struct_type = ret_type;
+	ret_type = Int64Ty;
+    }
+#endif
+
     Value *self_arg = NULL;
     if (is_objc) {
 	// self
@@ -6109,13 +6121,23 @@ RoxorCompiler::compile_stub(const char *types, bool variadic, int min_argc,
     }
 
     GetFirstType(types, buf, buf_len);
-    ret_type = convert_type(buf);
     if (self_arg != NULL && ret_type == VoidTy) {
 	// If we are calling an Objective-C method that returns void, let's
 	// return the receiver instead of nil, for convenience purposes.
 	retval = self_arg;
     }
     else {
+#if !__LP64__
+	if (small_struct_type != NULL) {
+	    Value *slot = new AllocaInst(small_struct_type, "", bb);
+	    new StoreInst(retval,
+		    new BitCastInst(slot, PointerType::getUnqual(ret_type),
+			"", bb),
+		    bb);
+	    retval = new LoadInst(slot, "", bb);
+	    ret_type = small_struct_type;
+	}
+#endif
 	retval = compile_conversion_to_ruby(buf, ret_type, retval);
     }
     ReturnInst::Create(context, retval, bb);

commit 3cac4f6281e007121da88ac28991ecab535a63c1

correct a problem introduced in previous commit (r4952)
git-svn-id: http://svn.macosforge.org/repository/ruby/MacRuby/trunk@4953 23306eb0-4c56-4727-a40e-e92c0eb68959

compiler.cpp
@@ -6024,7 +6024,7 @@ RoxorCompiler::compile_stub(const char *types, bool variadic, int min_argc,
 	ret_type = VoidTy;
     }
 
-#if !__LP64__
+#if !defined(__LP64__)
     const Type *small_struct_type = NULL;
     if (ret_type->getTypeID() == Type::StructTyID
 	    && GET_CORE()->get_sizeof(ret_type) == 8) {
@@ -6121,17 +6121,18 @@ RoxorCompiler::compile_stub(const char *types, bool variadic, int min_argc,
     }
 
     GetFirstType(types, buf, buf_len);
+    ret_type = convert_type(buf);
     if (self_arg != NULL && ret_type == VoidTy) {
 	// If we are calling an Objective-C method that returns void, let's
 	// return the receiver instead of nil, for convenience purposes.
 	retval = self_arg;
     }
     else {
-#if !__LP64__
+#if !defined(__LP64__)
 	if (small_struct_type != NULL) {
 	    Value *slot = new AllocaInst(small_struct_type, "", bb);
 	    new StoreInst(retval,
-		    new BitCastInst(slot, PointerType::getUnqual(ret_type),
+		    new BitCastInst(slot, PointerType::getUnqual(Int64Ty),
 			"", bb),
 		    bb);
 	    retval = new LoadInst(slot, "", bb);