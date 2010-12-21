b894d9b5c60fd7f518959fbee62357d77f8cf683
Watson <watson1978@gmail.com>
2010-12-21 15:35:37 +0000

    Kernel#untrust with frozen object will throw an exception.
    Test Script:
    {{{
    require 'test/unit/assertions.rb'
    include Test::Unit::Assertions
    
    o = Object.new
    assert_nothing_raised{ o.untrust }
    o.trust
    o.freeze
    assert_raise(RuntimeError){ o.untrust }
    
    puts :ok
    }}}
    
    git-svn-id: http://svn.macosforge.org/repository/ruby/MacRuby/trunk@5061 23306eb0-4c56-4727-a40e-e92c0eb68959

--- object.c
+++ object.c
@@ -1013,36 +1013,36 @@ static VALUE
 rb_obj_untrust_imp(VALUE obj, SEL sel)
 {
     rb_secure(4);
-    if (!SPECIAL_CONST_P(obj) && NATIVE(obj)) {
-	switch (TYPE(obj)) {
-	    case T_SYMBOL:
-		break;
-
-	    case T_ARRAY:
-		if (rb_klass_is_rary(*(VALUE *)obj)) {
-		    RBASIC(obj)->flags |= FL_UNTRUSTED;
-		    break;
-		}
-		// fall through
-	    case T_STRING:
-		if (rb_klass_is_rstr(*(VALUE *)obj)) {
-		    RBASIC(obj)->flags |= FL_UNTRUSTED;
-		    break;
-		}
-		// fall through
-	    case T_HASH:
-		if (rb_klass_is_rhash(*(VALUE *)obj)) {
-		    RBASIC(obj)->flags |= FL_UNTRUSTED;
-		    break;
-		}
-	    default:
-		rb_objc_flag_set((const void *)obj, FL_UNTRUSTED, true);
-	}
-	return obj;
-    }
-    if (!OBJ_TAINTED(obj)) {
+    if (!OBJ_UNTRUSTED(obj)) {
 	if (OBJ_FROZEN(obj)) {
 	    rb_error_frozen("object");
+	}
+	if (!SPECIAL_CONST_P(obj) && NATIVE(obj)) {
+	    switch (TYPE(obj)) {
+		case T_SYMBOL:
+		    break;
+
+		case T_ARRAY:
+		    if (rb_klass_is_rary(*(VALUE *)obj)) {
+			RBASIC(obj)->flags |= FL_UNTRUSTED;
+			break;
+		    }
+		    // fall through
+		case T_STRING:
+		    if (rb_klass_is_rstr(*(VALUE *)obj)) {
+			RBASIC(obj)->flags |= FL_UNTRUSTED;
+			break;
+		    }
+		    // fall through
+		case T_HASH:
+		    if (rb_klass_is_rhash(*(VALUE *)obj)) {
+			RBASIC(obj)->flags |= FL_UNTRUSTED;
+			break;
+		    }
+		default:
+		    rb_objc_flag_set((const void *)obj, FL_UNTRUSTED, true);
+	    }
+	    return obj;
 	}
 	FL_SET(obj, FL_UNTRUSTED);
     }
