351ebd767c260a85e9896f000629fc9df3397f45
Watson <watson1978@gmail.com>
2011-03-29 15:28:11 +0900

    Object#instance_exec will raise a TypeError when defining/alias methods on numerics.
    Test Script:
    {{{
    require 'test/unit/assertions.rb'
    include Test::Unit::Assertions
    
    assert_raise(TypeError){ 1.instance_exec { def foo; end } }
    assert_raise(TypeError){ 1.instance_exec { alias :foo :to_s} }
    
    puts :ok
    }}}

--- vm_eval.c
+++ vm_eval.c
@@ -561,7 +561,7 @@ rb_obj_instance_exec(VALUE self, SEL sel, int argc, VALUE *argv)
     VALUE klass;
 
     if (SPECIAL_CONST_P(self)) {
-	klass = 0;
+	klass = Qnil;
     }
     else {
 	klass = rb_singleton_class(self);
