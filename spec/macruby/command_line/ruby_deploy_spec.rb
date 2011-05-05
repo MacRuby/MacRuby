dd8fcef1bd1485241e122dd20501d5ef42285a92
Eloy Duran <eloy.de.enige@gmail.com>
2011-05-05 19:05:53 +0200

    Check that rbos actually work after compilation and that the original files are removed.

--- spec/macruby/command_line/ruby_deploy_spec.rb
+++ spec/macruby/command_line/ruby_deploy_spec.rb
@@ -1,33 +1,54 @@
 require File.expand_path('../../spec_helper', __FILE__)
 
-describe "The ruby_deploy --compile option" do
-  before do
-    dir = tmp('ruby_deploy')
-    FileUtils.mkdir_p dir
-    @app_bundle = File.join(dir, 'Dummy.app')
-    FileUtils.cp_r File.join(FIXTURES, 'Dummy.app'), @app_bundle
-  end
-
-  it "checks if the given path is a valid app bundle" do
-    FileUtils.rm_rf File.join(@app_bundle, 'Contents')
-    deploy('--compile').should include("doesn't seem to be a valid application bundle")
-  end
-
-  it "compiles the ruby source files in the app's Resources directory" do
-    deploy '--compile'
-    $?.success?.should == true
-    rbos = Dir.glob("#{@app_bundle}/Contents/Resources/**/*.rbo")
-    rbos.should_not be_empty
-    rbos.each { |rbo| file(rbo).should include('Mach-O') }
-  end
-
+module RubyDeploySpecHelper
   def deploy(args)
-    result = ruby_exe(File.join(SOURCE_ROOT, 'bin/ruby_deploy'), :args => "'#{@app_bundle}' #{args} 2>&1")
-    puts result
-    result
+    ruby_exe(File.join(SOURCE_ROOT, 'bin/ruby_deploy'), :args => "'#{@app_bundle}' #{args} 2>&1")
   end
 
   def file(path)
     `/usr/bin/file '#{path}'`
+  end
+end
+
+describe "ruby_deploy, in general," do
+  extend RubyDeploySpecHelper
+
+  it "checks if the given path is a valid app bundle" do
+    @app_bundle = tmp('ruby_deploy/Dummy.app')
+    FileUtils.mkdir_p @app_bundle
+    deploy('--compile').should include("doesn't seem to be a valid application bundle")
+  end
+end
+
+describe "The ruby_deploy --compile option" do
+  extend RubyDeploySpecHelper
+
+  before do
+    dir = tmp('ruby_deploy')
+    FileUtils.mkdir_p dir
+    @app_bundle = File.join(dir, 'Dummy.app')
+    FileUtils.cp_r File.join(FIXTURES, 'dummy_app'), @app_bundle
+    # we just need a binary file compiled in the arch for the current env
+    FileUtils.mkdir File.join(@app_bundle, 'Contents/MacOS')
+    FileUtils.cp File.join(SOURCE_ROOT, 'miniruby'), File.join(@app_bundle, 'Contents/MacOS/Dummy')
+  end
+
+  it "compiles the ruby source files in the app's Resources directory" do
+    deploy('--compile')
+    rbos = Dir.glob("#{@app_bundle}/Contents/Resources/**/*.rbo")
+    rbos.should_not be_empty
+    rbos.each do |rbo|
+      file(rbo).should include('Mach-O')
+      require rbo
+    end
+    # check that the classes defined in the rbos actually work
+    defined?(DummyModel).should == "constant"
+    defined?(DummyController).should == "constant"
+  end
+
+  it "removes the original source files after compilation" do
+    deploy('--compile')
+    rbs = Dir.glob("#{@app_bundle}/Contents/Resources/**/*.rb")
+    rbs.should be_empty
   end
 end
