840306111481d71149b7c38524cb943bf337a136
Kouji Takao <kouji@takao7.net>
2011-06-01 14:37:15 +0900

    fixed bug below. --- class EvalSpecs   class A     def c       eval "class C; end"     end   end end
    EvalSpecs::A.new.c
    
    p EvalSpecs::A::C.name #=> "EvalSpecs::A::C"

--- vm.cpp
+++ vm.cpp
@@ -1467,12 +1467,8 @@ rb_vm_define_class(ID path, VALUE outer, VALUE super, int flags,
     rb_vm_check_if_module(outer);
 
     if (flags & DEFINE_OUTER) {
-	rb_vm_outer_t *o = outer_stack;
-	while (o != NULL) {
-	    o = o->outer;
-	}
-	if (o != NULL) {
-	    outer = (VALUE)o->klass;
+	if (outer_stack != NULL) {
+	    outer = (VALUE)outer_stack->klass;
 	}
     }
 
