d5d1e6f294dacf658b7c9fa4e3ae2effe7449315
Watson <watson1978@gmail.com>
2011-06-01 20:58:00 +0900

    reduce the memory usage in IO#read.
    Result: (file size = 100 MB)
    ----
    case     before      after
    -------------------------------
    case 1 : 426,120 KB  320,524 KB
    case 2 : 117,312 KB   11,996 KB
    
    {{{
    def memory
      pid = $$
      str = `ps alx | grep '#{pid}.*ruby'`
      str.split[7]
    end
    
    File.open("/tmp/foo", "r") {|f|
      case ARGV[0].to_i
      when 1
        # case 1
        f.read
      when 2
        # case 2
        f.read(1024 * 1024)
      else
        raise "\n******** chose 1 or 2 ********\n"
      end
    }
    
    puts memory + " KB"
    }}}
    
    {{{
    require 'test/unit/assertions.rb'
    include Test::Unit::Assertions
    
    str = "foo\nbar\nbaz0123456789"
    IO.pipe {|r, w|
      w.write str
      w.close
      assert_equal(str, r.read)
    }
    
    IO.pipe {|r, w|
      w.write str
      w.close
      assert_equal("fo", r.read(2))
    }
    
    require 'socket'
    
    svr = TCPServer.new("localhost", 0)
    th = Thread.new {
      loop do
        c = svr.accept
        c.write str
        c.close
      end
    }
    addr = svr.addr
    
    sock = TCPSocket.open(addr[3], addr[1])
    assert_equal(str, sock.read)
    sock.close
    
    sock = TCPSocket.open(addr[3], addr[1])
    assert_equal("fo", sock.gets("\n", 2))
    assert_equal("o\nbar\nbaz0123456789", sock.read)
    sock.close
    
    puts :ok
    }}}

--- io.c
+++ io.c
@@ -1069,7 +1069,13 @@ rb_io_read_all(rb_io_t *io_struct, VALUE outbuf)
     while (true) {
 	rb_bstr_resize(outbuf, original_position + bytes_read + bufsize);
 	uint8_t *bytes = rb_bstr_bytes(outbuf) + original_position + bytes_read;
-        const long last_read = rb_io_read_internal(io_struct, bytes, bufsize);
+        long last_read;
+	if (io_struct->buf != NULL) {
+	    last_read = rb_io_read_internal(io_struct, bytes, bufsize);
+	}
+	else {
+	    last_read = read_internal(io_struct->read_fd, (UInt8 *)bytes, bufsize);
+	}
         bytes_read += last_read;
 	if (last_read == 0) {
 	    break;
@@ -1341,7 +1347,14 @@ io_read(VALUE io, SEL sel, int argc, VALUE *argv)
     rb_bstr_resize(outbuf, size);
     uint8_t *bytes = rb_bstr_bytes(outbuf);
 
-    const long data_read = rb_io_read_internal(io_struct, bytes, size);
+    long data_read;
+    if (io_struct->buf != NULL) {
+	data_read = rb_io_read_internal(io_struct, bytes, size);
+    }
+    else {
+	data_read = read_internal(io_struct->read_fd, (UInt8 *)bytes, size);
+    }
+
     rb_bstr_set_length(outbuf, data_read);
 
     if (data_read == 0) {
