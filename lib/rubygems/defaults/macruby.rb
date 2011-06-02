f1ef1ffceef8a3a515fa5aed8916c72a18145443
Mark Rada <mrada@marketcircle.com>
2011-06-02 02:45:16 -0400

    Define a MacRuby-only platform for gems to use

--- lib/rubygems/defaults/macruby.rb
+++ lib/rubygems/defaults/macruby.rb
@@ -3,4 +3,9 @@ require 'rubygems/config_file'
 module Gem
   ConfigFile::PLATFORM_DEFAULTS['install'] = '--no-rdoc --no-ri'
   ConfigFile::PLATFORM_DEFAULTS['update']  = '--no-rdoc --no-ri'
+
+  Platform::MACRUBY         = Platform.new([Platform.local.cpu, RUBY_ENGINE])
+  Platform::MACRUBY_CURRENT = Platform.new([Platform.local.cpu, RUBY_ENGINE,
+                                            MACRUBY_VERSION.split('.')[0, 2].join('.')])
+  platforms << Platform::MACRUBY_CURRENT
 end
