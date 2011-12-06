5565532a7b2e16dae01cda2407b7c828b9625de7
Watson <watson1978@gmail.com>
2011-12-07 08:26:34 +0900

    add test case in sendfile's spec

--- spec/macruby/library/socket/sendfile_spec.rb
+++ spec/macruby/library/socket/sendfile_spec.rb
@@ -2,42 +2,53 @@ require 'socket'
 
 describe "Socket" do
   describe :sendfile do
+
+    before :each do
+      @port = 2112
+      @server = TCPServer.open(@port)
+      @server.listen(5)
+      @socket = TCPSocket.open('localhost', @port)
+    end
+
+    after :each do
+      @socket.close
+      @server.close
+    end
     
     it "should be able to send instances of IO" do
-      port = 2112
       io = File.open(File.dirname(__FILE__) + '/fixtures/sample.txt')
-      server = TCPServer.open(port)
-      socket = TCPSocket.open('localhost', port)
       
       Dispatch::Queue.concurrent.async do
-        server.listen(5)
-        client = server.accept
+        client = @server.accept
         client.sendfile(io, 0, io.stat.size)
         client.close
       end
       
-      socket.readlines.should == io.readlines
-      socket.close
-      server.close
+      @socket.readlines.should == io.readlines
     end
     
     it "should be able to send files specified by a path" do
-      port = 2112
       path = File.dirname(__FILE__) + '/fixtures/sample.txt'
       io = File.open(path)
-      server = TCPServer.open(port)
-      socket = TCPSocket.open('localhost', port)
       
       Dispatch::Queue.concurrent.async do
-        server.listen(5)
-        client = server.accept
+        client = @server.accept
         client.sendfile(path, 0, io.stat.size)
         client.close
       end
       
-      socket.readlines.should == io.readlines
-      socket.close
-      server.close
+      @socket.readlines.should == io.readlines
     end
+
+   it "should raise if wrong value passed" do
+      path = File.dirname(__FILE__) + '/fixtures/sample.txt'
+
+      client = @server.accept
+      lambda{ client.sendfile(Object.new, 0, 1) }.should raise_error(TypeError)
+      lambda{ client.sendfile(path, Object.new, 1) }.should raise_error(TypeError)
+      lambda{ client.sendfile(path, 0, Object.new) }.should raise_error(TypeError)
+      client.close
+    end
+
   end
 end
