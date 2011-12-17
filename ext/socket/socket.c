efbe15965b433697b6b746eb5285b5bea6e53b68
Watson <watson1978@gmail.com>
2011-12-17 11:17:30 +0900

    ext/socket: add constant_arg, family_arg, socktype_arg, level_arg, optname_arg and shutdown_how_arg

--- ext/socket/socket.c
+++ ext/socket/socket.c
@@ -235,6 +235,80 @@ ruby_getnameinfo__aix(const struct sockaddr *sa, size_t salen,
     } \
     while (0)
 
+static int
+constant_arg(VALUE arg, int (*str_to_int)(char*, int, int*), const char *errmsg)
+{
+    VALUE tmp;
+    char *ptr;
+    int ret;
+
+    if (SYMBOL_P(arg)) {
+        arg = rb_sym_to_s(arg);
+        goto str;
+    }
+    else if (!NIL_P(tmp = rb_check_string_type(arg))) {
+	arg = tmp;
+      str:
+	rb_check_safe_obj(arg);
+        ptr = (char *)RSTRING_PTR(arg);
+        if (str_to_int(ptr, RSTRING_LEN(arg), &ret) == -1)
+	    rb_raise(rb_eSocket, "%s: %s", errmsg, ptr);
+    }
+    else {
+	ret = NUM2INT(arg);
+    }
+    return ret;
+}
+
+static int
+family_arg(VALUE domain)
+{
+    /* convert AF_INET, etc. */
+    return constant_arg(domain, family_to_int, "unknown socket domain");
+}
+
+static int
+socktype_arg(VALUE type)
+{
+    /* convert SOCK_STREAM, etc. */
+    return constant_arg(type, socktype_to_int, "unknown socket type");
+}
+
+static int
+level_arg(VALUE level)
+{
+    /* convert SOL_SOCKET, IPPROTO_TCP, etc. */
+    return constant_arg(level, level_to_int, "unknown protocol level");
+}
+
+static int
+optname_arg(int level, VALUE optname)
+{
+    switch (level) {
+      case SOL_SOCKET:
+        return constant_arg(optname, so_optname_to_int, "unknown socket level option name");
+      case IPPROTO_IP:
+        return constant_arg(optname, ip_optname_to_int, "unknown IP level option name");
+#ifdef IPPROTO_IPV6
+      case IPPROTO_IPV6:
+        return constant_arg(optname, ipv6_optname_to_int, "unknown IPv6 level option name");
+#endif
+      case IPPROTO_TCP:
+        return constant_arg(optname, tcp_optname_to_int, "unknown TCP level option name");
+      case IPPROTO_UDP:
+        return constant_arg(optname, udp_optname_to_int, "unknown UDP level option name");
+      default:
+        return NUM2INT(optname);
+    }
+}
+
+static int
+shutdown_how_arg(VALUE how)
+{
+    /* convert SHUT_RD, SHUT_WR, SHUT_RDWR. */
+    return constant_arg(how, shutdown_how_to_int, "unknown shutdown argument");
+}
+
 static VALUE
 init_sock(VALUE sock, int fd)
 {
