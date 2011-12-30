cf5eae4a96e74885533bcab851d794c22e57f8dc
Watson <watson1978@gmail.com>
2011-12-30 16:40:15 +0900

    does not invoke initialize_copy in init_copy()

--- object.c
+++ object.c
@@ -217,14 +217,13 @@ init_copy(VALUE dest, VALUE obj)
 	    ROBJECT(dest)->num_slots = ROBJECT(obj)->num_slots;
 	    break;
     }
-
-    rb_vm_call(dest, selInitializeCopy, 1, &obj);
 }
 
 void
 rb_obj_invoke_initialize_copy(VALUE dest, VALUE obj)
 {
     init_copy(dest, obj);
+    rb_vm_call(dest, selInitializeCopy, 1, &obj);
 }
 
 /*
