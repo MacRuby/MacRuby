607cc509ad31169d6e2259a7e0dc167293949402
Watson <watson1978@gmail.com>
2012-01-04 11:24:39 +0900

    use long for seekdir()

--- dir.c
+++ dir.c
@@ -582,7 +582,7 @@ static VALUE
 dir_seek(VALUE dir, SEL sel, VALUE pos)
 {
     struct dir_data *dirp;
-    off_t p = NUM2OFFT(pos);
+    long p = NUM2LONG(pos);
 
     GetDIR(dir, dirp);
     seekdir(dirp->dir, p);
