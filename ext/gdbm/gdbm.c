7b4cd8dca0dd587663bea0be1371b802e0195eb4
Watson <watson1978@gmail.com>
2013-01-04 17:19:59 +0900

    [fix #179] use standard memory allocation functions in gdbm

--- ext/gdbm/gdbm.c
+++ ext/gdbm/gdbm.c
@@ -10,6 +10,19 @@
 ************************************************/
 
 #include "ruby.h"
+#ifdef __MACRUBY__
+/* We cannot use the GC memory functions here
+ * function will call free() on the memory, resulting in a leak.
+ */
+# undef ALLOC
+# define ALLOC(type) (type*)malloc(sizeof(type))
+# undef ALLOC_N
+# define ALLOC_N(type,n) ((type *)malloc(sizeof(type) * (n)))
+# undef REALLOC_N
+# define REALLOC_N(var,type,n) \
+    (var)=(type*)realloc((char*)(var),(n) * sizeof(type))
+# define ruby_xfree(x) free(x)
+#endif
 
 #include <gdbm.h>
 #include <fcntl.h>
