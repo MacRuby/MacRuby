8db7332057ec5281eecaf3e2c9463c58371e9b1d
Watson <watson1978@gmail.com>
2013-01-09 16:48:28 +0900

    Revert "enable ext/gdbm", because OSX does not bundle gdbm library
    This reverts commit 8a2cabf43362ff2ac6ccd8531a890a9a657e7bbe.

--- rakelib/builder/builder.rb
+++ rakelib/builder/builder.rb
@@ -5,7 +5,7 @@ require File.expand_path('../options', __FILE__)
 
 EXTENSIONS = %w{
   ripper digest etc readline libyaml fcntl socket zlib bigdecimal openssl json
-  nkf iconv io gdbm
+  nkf iconv io
 }.sort
 
 class Builder
